d999932ca9bbc0096ab0d9ca43c568a3dbdf75ab
Vanessa <vanessamrios@gmail.com>
2021-07-22 17:37:54 -0300

    Atende aos resquisitos 6 e 7

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -3,9 +3,11 @@ import React from 'react';
 
 class MovieCard extends React.Component {
   render() {
+    const { movie } = this.props;
+    const { title, subtitle, storyline, imagePath, rating } = movie;
     return (
       <section>
-        <p>test</p>
+        <img src={ imagePath } alt={ title } />
       </section>
     );
   }
--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -6,7 +6,6 @@ import MovieCard from './MovieCard';
 class MovieList extends React.Component {
   render() {
     const { movies } = this.props;
-    console.log(movies);
     return (
       <div>
         {movies.map((movie) => <MovieCard key={ movie.title } movie={ movie } />)}
