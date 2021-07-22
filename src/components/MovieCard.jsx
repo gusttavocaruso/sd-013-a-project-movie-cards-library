7cd5587c8e21f505626927bf494c3083b9b3ffc0
Vanessa <vanessamrios@gmail.com>
2021-07-22 17:05:31 -0300

    Atende aos requisitos 4 e 5

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,1 +1,14 @@
 // implement MovieCard component here
+import React from 'react';
+
+class MovieCard extends React.Component {
+  render() {
+    return (
+      <section>
+        <p>test</p>
+      </section>
+    );
+  }
+}
+
+export default MovieCard;
