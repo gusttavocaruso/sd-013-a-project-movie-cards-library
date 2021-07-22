a065dc222b81c9b059e356f7c102694002fc7f25
LuizFJP <luizfjportela@gmail.com>
2021-07-22 19:15:16 -0300

    requirement 10

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -6,14 +6,10 @@ class MovieCard extends Component {
     // const { title } = movies;
     return (
       <div>
+        <img src={ imagePath } alt={ title } />
         <h4>{title}</h4>
         <h5>{ subtitle }</h5>
-        <img src={ imagePath } alt={ title } />
-        <p>{ title }</p>
-        <p>{ subtitle }</p>
         <p>{ storyline }</p>
-        <p>{ imagePath }</p>
-        <p>{ rating }</p>
       </div>
     );
   }
