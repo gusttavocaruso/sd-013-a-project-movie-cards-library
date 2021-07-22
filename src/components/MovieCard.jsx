7dffa6cacd1d20a385af7ac03ce9d98416538391
Gustavo Pia <piagja@gmail.com>
2021-07-22 18:27:44 -0300

    Arrumou CSS para o Rating

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -16,9 +16,9 @@ class MovieCard extends Component {
           <h4 className="movie-card-title">{title}</h4>
           <h5 className="movie-card-subtitle">{subtitle}</h5>
           <p className="movie-card-storyline">{storyline}</p>
-          <div className="movie-card-rating">
-            <Rating rating={ rating } />
-          </div>
+        </div>
+        <div className="movie-card-rating">
+          <Rating rating={ rating } />
         </div>
       </section>
     );
