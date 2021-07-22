6150a2707ff3680f14f5e0814f634fd68e32f88c
Jaqueline Silva <jaqueline.natario@gmail.com>
2021-07-22 17:48:08 -0300

    Resolve o requisito 8

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -2,11 +2,12 @@ import React from 'react';
 
 class MovieCard extends React.Component {
   render() {
-    const { movie } = this.props;
+    const { movie } = this.props; // linha 10 MovieList
     const { title, subtitle, storyLine, imagePath, rating } = movie;
     return (
       <section>
         <img src={ imagePath } alt={ title } />
+        <h4>{ title }</h4>
       </section>
     );
   }
