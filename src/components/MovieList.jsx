9444443849cf3c33808edc0d94ff50124765de91
Rafhael Gomes <rafhael.andrade@hotmail.com.br>
2021-07-22 16:02:34 -0300

    sei la

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,1 +1,18 @@
-// implement MovieList component here
+import React from 'react';
+import MovieCard from './MovieCard';
+import movies from '../data';
+
+export default class MovieList extends React.Component {
+  render() {
+    const moviesMap = movies
+      .map((obj) => (<MovieCard
+        movie={ obj }
+        key={ obj.title }
+      />));
+
+    return (
+      <main className="movie-list">
+        { moviesMap }
+      </main>);
+  }
+}
--- src/components/Rating.jsx
+++ src/components/Rating.jsx
@@ -1,1 +1,19 @@
-// implement Rating component here
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+export default class Rating extends Component {
+  render() {
+    const { rating } = this.props;
+    return (
+      <h4 className="rating">{rating}</h4>
+    );
+  }
+}
+
+Rating.propTypes = {
+  rating: PropTypes.number,
+};
+
+Rating.defaultProps = {
+  rating: 0,
+};
