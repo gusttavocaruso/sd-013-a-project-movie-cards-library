e940129d6520d55adea4160c562dac8ea51cd485
Fernando Costa <fgosuendacosta@gmail.com>
2021-07-22 14:15:56 -0300

    Iniciando projeto

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,1 +1,13 @@
-// implement Header component here
+import React from 'react';
+
+class Header extends React.Component {
+  render() { // Chama o método para renderizar, criando o próprio método render
+    return ( // O return tem que retornar um HTML
+      <header className="movie-card-header">
+        <h1 className="page-title">Movie Cards Library</h1>
+      </header>
+    );
+  }
+}
+
+export default Header; // Sempre exportar o componente
