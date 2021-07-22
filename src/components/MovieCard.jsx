0f2cf7a865fff3ed0cca8ec2daaf8993325b6258
Demistocles <demi.treze@gmail.com>
2021-07-22 15:59:17 -0300

    iniciando projeto

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,1 +1,30 @@
-// implement MovieCard component here
+import React from 'react';
+// import propTypes from 'prop-types';
+import movies from '../data';
+
+class MovieCard extends React.Component {
+  render() {
+    const mapMovies = movies.map((movie, index) => (
+      <div key={ index }>
+        <img src={ movie.imagePath } alt={ movie.title } />
+        <h4>{ movie.title }</h4>
+        <h5>{ movie.subtitle }</h5>
+        <p>{ movie.storyline }</p>
+      </div>
+    ));
+
+    return (
+      <div>
+        {mapMovies}
+      </div>
+    );
+  }
+}
+
+/* MovieCard.propTypes = {
+  movies: propTypes.object.isRequired,
+  imagePath: propTypes.string.isRequired,
+  title: propTypes.string.isRequired,
+}; */
+
+export default MovieCard;
