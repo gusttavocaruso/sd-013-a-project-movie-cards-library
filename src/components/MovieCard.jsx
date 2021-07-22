6b85ccea8cda3a247378ba5ad12f4a31e8f76736
ALINE <hoshinoaline@yahoo.com.br>
2021-07-22 16:13:16 -0300

    requisito 3

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,1 +1,15 @@
-// implement MovieCard component here
+// // implement MovieCard component here
+// import React from 'react';
+// import PropType from 'prop-types';
+// import Rating from './Rating';
+
+// class MovieCard extends React.Component {
+//   render() {
+//     const { movie=  } = this.props;
+//     return (
+//       <div>
+
+//       </div>
+//     );
+//   }
+// }
