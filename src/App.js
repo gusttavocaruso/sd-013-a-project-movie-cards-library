903ee6de985833c7469be9b8e6423f15cb7ea0cc
Gabriel Biasoli <bielbiasoli33@icloud.com>
2021-07-22 15:37:12 -0300

    Finaliza requisitos 5 e 6

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './App.css';
-import movies from './data';
 import MovieList from './components/MovieList';
+import movies from './data';
 
 function App() {
   return (
--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -2,6 +2,12 @@ import React from 'react';
 
 class MovieCard extends React.Component {
   render() {
+    const { movie } = this.props;
+    return (
+      <div>
+        <h1>{movie.title}</h1>
+      </div>
+    );
   }
 }
 
