852812c9fd5f84028788c3ef35ec200e0198c4fa
CarlosLima <carlos.augustolimaf@gmail.com>
2021-07-22 18:48:58 -0300

    Começando o projeto com os requisitos 1 e 2

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React, { Components } from 'react';
+
+class Header extends Components {
+  render() {
+    return(
+      <header>
+        <h1>Movie Cards Library</h1>
+      </header>
+    );
+  }
+}
