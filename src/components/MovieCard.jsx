9803ca3ccd25369b38a1f5797011495db30eb950
thomasravache <thomasravache31@gmail.com>
2021-07-22 19:42:00 -0300

    Adicionando classes CSS para os componentes

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -6,14 +6,14 @@ class MovieCard extends React.Component {
   render() {
     const { movie: { title, subtitle, storyline, rating, imagePath } } = this.props;
     return (
-      <div>
+      <div className="movie-card">
         <div>
-          <img src={ imagePath } alt={ title } />
+          <img className="movie-card-image" src={ imagePath } alt={ title } />
         </div>
-        <div>
-          <h4>{ title }</h4>
-          <h5>{ subtitle }</h5>
-          <p>{ storyline }</p>
+        <div className="movie-card-body">
+          <h4 className="movie-card-title">{ title }</h4>
+          <h5 className="movie-card-subtitle">{ subtitle }</h5>
+          <p className="movie-card-storyline">{ storyline }</p>
           <Rating rating={ rating } />
         </div>
       </div>
