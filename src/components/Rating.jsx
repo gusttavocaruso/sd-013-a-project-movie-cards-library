7a03ab9badb05747f0fd4d602165abf5de356bf5
Juliane Marques <mqs.juliane@gmail.com>
2021-07-22 17:43:48 -0300

    requisito 12

--- src/components/Rating.jsx
+++ src/components/Rating.jsx
@@ -1,2 +1,14 @@
 // implement Rating component here
 import React from 'react';
+
+class Rating extends React.Component {
+    render() {
+        const { rating } = this.props;
+        return (
+            
+        )
+
+    }
+}
+
+export default Rating;
