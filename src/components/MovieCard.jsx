a9836485dd0b01ede8ea4e47f4cb3ffd8e9275d2
Isabella Antunes <bella.antunes2@gmail.com>
2021-07-22 17:43:48 -0300

    Requisitos 9 e 10

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -8,6 +8,8 @@ class MovieCard extends React.Component {
       <section>
         <img src={ imagePath } alt={ title } />
         <h4>{ title }</h4>
+        <h5>{ subtitle }</h5>
+        <p>{ storyline }</p>
       </section>
     );
   }
