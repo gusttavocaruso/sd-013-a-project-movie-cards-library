aa361cccc4872e594debf544cdf6fbd3f72b891c
V. Dionysio <vini.dionysio@gmail.com>
2021-07-22 16:14:22 -0300

    reqs 3 and 16 - solving lint

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,16 +1,27 @@
 // implement MovieList component here
 import React, { Component } from 'react';
+// import PropTypes from 'prop-types';
 
 class MovieList extends Component {
   render() {
-    const { movies } = this.props;
+    // const { movies } = this.props;
 
     return (
       <div>
-        {movies.map((movie) => <h1>movie</h1>)}
+        {/* {movies.map((movie) => <h1>movie</h1>)} */}
       </div>
     );
   }
 }
+
+// MovieList.PropTypes = {
+//   movies: PropTypes.arrayOf(PropTypes.shape({
+//     title: PropTypes.string,
+//     subtitle: PropTypes.string,
+//     storyline: PropTypes.string,
+//     rating: PropTypes.number,
+//     imagePath: PropTypes.string,
+//   })).isRequired,
+// };
 
 export default MovieList;
