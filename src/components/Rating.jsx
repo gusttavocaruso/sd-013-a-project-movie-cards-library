23fd6d934e4918dc9095427d79a76f3af8d32c65
Fernando Costa <fgosuendacosta@gmail.com>
2021-07-22 14:22:19 -0300

    Realizando primeiros requisitos 15

--- src/components/Rating.jsx
+++ src/components/Rating.jsx
@@ -1,1 +1,18 @@
-// implement Rating component here
+// Requisito 11 - Crie um componente <Rating />
+import React from 'react';
+import PropTypes from 'prop-types'; // Importando o PropTypes
+
+class Rating extends React.Component {
+  render() { // Chama o método para renderizar, criando o próprio método render
+    // Recebo a props movie que é um objeto do array movies
+    // Desestruturo buscando o rating dentro do movies
+    const { movie: { rating } } = this.props;
+
+    // Renderizo abaixo a props movie que foi desestruturada
+    return ( // O return tem que retornar um HTML
+      <div className="movie-card-rating">
+        <span className="rating">{ rating }</span>
+      </div>
+    );
+  }
+}
