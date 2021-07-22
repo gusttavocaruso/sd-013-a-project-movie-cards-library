50db4ceb9089431e995710714d38e5c539f274e7
Mateus Montagnini <mateus.r.montagnini@gmail.com>
2021-07-22 17:25:05 -0300

    Resolve requisitos 3, 4 e 5

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,1 +1,31 @@
-// implement MovieCard component here
+import React from 'react';
+import PropType from 'prop-types';
+
+class MovieCard extends React.Component {
+  render() {
+    const { movieInfo } = this.props;
+    const { imagePath, title, subtitle, storyline, rating } = movieInfo;
+
+    return (
+      <div>
+        <img src={ imagePath } alt={ title } />
+        <h3>{ title }</h3>
+        <h4>{ subtitle }</h4>
+        <p>{ storyline }</p>
+        <p>{ rating }</p>
+      </div>
+    );
+  }
+}
+
+MovieCard.propTypes = {
+  movieInfo: PropType.shape({
+    title: PropType.string,
+    subtitle: PropType.string,
+    storyline: PropType.string,
+    rating: PropType.number,
+    imagePath: PropType.string,
+  }).isRequired,
+};
+
+export default MovieCard;
--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,1 +1,20 @@
-// implement MovieList component here
+import React from 'react';
+import PropType from 'prop-types';
+import MovieCard from './MovieCard';
+
+class MovieList extends React.Component {
+  render() {
+    const { movies } = this.props;
+
+    return (
+      movies.map((movie) => (
+        <MovieCard key={ movie.title } movieInfo={ movie } />))
+    );
+  }
+}
+
+MovieList.propTypes = {
+  movies: PropType.arrayOf(PropType.object).isRequired,
+};
+
+export default MovieList;
