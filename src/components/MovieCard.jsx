d75372729fbb9d9809fbda09dda6bc03cad28b2c
Lanai Conceição <conceicao.lanai@gmail.com>
2021-07-22 17:58:08 -0300

    Requisito 10

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -3,12 +3,13 @@ import React from 'react';
 class MovieCard extends React.Component {
   render() {
     const { movie } = this.props;
-    const { imagePath, title, subtitle } = movie;
+    const { imagePath, title, subtitle, storyline } = movie;
     return (
       <div>
         <img src={ imagePath } alt={ title } />
         <h4>{ title }</h4>
         <h5>{ subtitle }</h5>
+        <p>{ storyline }</p>
       </div>
     );
   }
