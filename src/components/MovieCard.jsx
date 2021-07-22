b2609dcd2c6faddc7626175c2d18d5ccb4ab307d
João Paulo <jp.viegas@hotmail.com>
2021-07-22 15:48:50 -0300

    lint fix

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,1 +1,13 @@
 // implement MovieCard component here
+import React from 'react';
+import movies from '../data';
+
+class MovieCard extends React.Component {
+  render() {
+    return (
+      movies.title
+    );
+  }
+}
+
+export default MovieCard;
--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,1 +1,14 @@
 // implement MovieList component here
+import React from 'react';
+
+class MovieList extends React.Component {
+  render() {
+    return (
+      <header>
+        <h1>movie list</h1>
+      </header>
+    );
+  }
+}
+
+export default MovieList;
