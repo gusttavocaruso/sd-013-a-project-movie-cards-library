7eee13c49cabb3b35e891609b98a9a1b11469ede
Julio Fallace <julioscoffee@gmail.com>
2021-07-22 19:26:18 -0300

    resolvendo até requisito 10, rever alguns. Variavel 'rating' removida temporariamente por causa do linter

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,9 +1,29 @@
+import PropTypes from 'prop-types';
 import React from 'react';
 
 class MovieCard extends React.Component {
   render() {
-    return;
+    const { movie } = this.props;
+    const { title, subtitle, storyline, imagePath } = movie;
+    return (
+      <div>
+        <img src={ imagePath } alt={ title } />
+        <h4>{ title }</h4>
+        <h5>{ subtitle }</h5>
+        <p>{ storyline }</p>
+      </div>
+    );
   }
 }
+
+MovieCard.propTypes = {
+  movie: PropTypes.shape({
+    title: PropTypes.string,
+    subtitle: PropTypes.string,
+    storyline: PropTypes.string,
+    imagePath: PropTypes.string,
+    rating: PropTypes.number,
+  }).isRequired,
+};
 
 export default MovieCard;
--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,16 +1,20 @@
+import PropTypes from 'prop-types';
 import React from 'react';
 import MovieCard from './MovieCard';
 
 class MovieList extends React.Component {
   render() {
     const { movies } = this.props;
-    
     return (
       <div>
-        { movies.map((movie) => <MovieCard />) }
+        { movies.map((movie) => <MovieCard key={ movie.name } />) }
       </div>
     );
   }
 }
+
+MovieList.propTypes = {
+  movies: PropTypes.arrayOf(PropTypes.object).isRequired,
+};
 
 export default MovieList;
