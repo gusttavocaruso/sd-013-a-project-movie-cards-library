bd5b1522c01b69549480af06ba57299087e0bab2
luiz furtado <luizfurtadomkt@hotmail.com>
2021-07-22 17:22:56 -0300

    default prop rating

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
   return (
     <div className="App">
       <Header />
-      <MovieList movies={ movies } />
+      <MovieList className="movie-list" movies={ movies } />
     </div>
   );
 }
--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import MovieCard from './MovieCard';
+import PropTypes from 'prop-types'
 
 class MovieList extends React.Component {
   render() {
     const { movies } = this.props;
     return (
       <div>
-        { movies.map((movie) => <MovieCard key={ movie.title } movie={ movie } />) }
+        { movies.map((movie) => <MovieCard className="movie-card" key={ movie.title } movie={ movie } />) }
       </div>
     );
   }
