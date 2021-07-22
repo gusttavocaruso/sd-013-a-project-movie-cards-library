b04c2c5919095629e018edd880cbd704d1f6d5cc
Pedro Delicoli <pedrodelicoli@hotmail.com>
2021-07-22 18:45:06 -0300

    Requisito 6-16

--- src/App.js
+++ src/App.js
@@ -2,16 +2,14 @@ import React from 'react';
 import './App.css';
 import Header from './components/Header';
 import MovieList from './components/MovieList';
-import Data from './data';
+import Movies from './data';
 
 class App extends React.Component {
   render() {
     return (
       <div className="App">
-        <header>
-          <h1 className="page-title"><Header /></h1>
-        </header>
-        <MovieList movies={ Data } />
+        <Header />
+        <MovieList movies={ Movies } />
       </div>
     );
   }
