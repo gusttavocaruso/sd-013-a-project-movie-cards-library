38066e6e72a6f38e333d7fe1ab73e877a2c19ea2
Jéssica Queiroz <gnr0034@gmail.com>
2021-07-22 17:03:56 -0300

    Requisito 4

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,1 +1,14 @@
 // implement MovieCard component here
+import React from 'react';
+
+class MovieCard extends React.Component {
+  render() {
+    return (
+      <div>
+        <p> </p>
+      </div>
+    );
+  }
+}
+
+export default MovieCard;
--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,13 +1,21 @@
 // implement MovieList component here
 import React from 'react';
-import movies from '../data';
+import PropTypes from 'prop-types';
+import MovieCard from './MovieCard';
 
 class MovieList extends React.Component {
   render() {
+    const { movies } = this.props;
     return (
-      <div>{movies}</div>
+      <div>
+        { movies.map((movie) => <MovieCard key={ movie.title } infoMovie={ movie } />) }
+      </div>
     );
   }
 }
+
+MovieList.propTypes = {
+  movies: PropTypes.arrayOf(PropTypes.object).isRequired,
+};
 
 export default MovieList;
