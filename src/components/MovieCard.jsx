ee2400029bbc7a3840eca6a3f6d26323cffd6330
thomasravache <thomasravache31@gmail.com>
2021-07-22 17:39:58 -0300

    5 - Passe uma key para cada <MovieCard /> renderizado

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -2,16 +2,16 @@ import React from 'react';
 
 class MovieCard extends React.Component {
   render() {
-    const { movies } = this.props;
+    const { title, subtitle, storyline, rating, imagePath } = this.props.dataMovie;
     return(
       <div>
         <div>
-          <img src={movies.imagePath} alt={movies.title} />
+          <img src={imagePath} alt={title} />
         </div>
         <div>
-          <h3>{movies.title}</h3>
-          <h4>{movies.subtitle}</h4>
-          <p>{movies.storyline}</p>
+          <h3>{title}</h3>
+          <h4>{subtitle}</h4>
+          <p>{storyline}</p>
         </div>
       </div>
     );
--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import MovieCard from './MovieCard'
-import movies from '../data';
 
 class MovieList extends React.Component {
   render() {
+    const { movies } = this.props;
+
     return (
       <section>
-        {movies.map((movie) => <MovieCard movies={movie} />)}
+        { movies.map((movie) => <MovieCard dataMovie={ movie } />) }
       </section>
     );
   }
