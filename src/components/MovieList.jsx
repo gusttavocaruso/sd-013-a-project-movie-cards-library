2218437fa7405fcde9ea2045e2edaffbc2ba9a2b
gustavo.caruso <gustavoalmeida26@hotmail.com>
2021-07-22 16:42:54 -0300

    update in the project

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,1 +1,14 @@
-// implement MovieList component here
+import React from 'react';
+// import movies from '../data';
+
+class MovieList extends React.Component {
+  render() {
+    return (
+      <div>
+        not-yet
+      </div>
+    );
+  }
+}
+
+export default MovieList;
