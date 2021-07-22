bc08e8ca64ef2f31ab746a9651835055e7448119
Pedro Delicoli <pedrodelicoli@hotmail.com>
2021-07-22 15:55:41 -0300

    Teste

--- src/App.js
+++ src/App.js
@@ -5,11 +5,7 @@ import Header from './components/Header';
 class App extends React.Component {
   render() {
     return (
-      <div className="App">
-        <header className="movie-card-header">
-          <h1 className="page-title"><Header /></h1>
-        </header>
-      </div>
+      <div className="App"> </div>
     );
   }
 }
