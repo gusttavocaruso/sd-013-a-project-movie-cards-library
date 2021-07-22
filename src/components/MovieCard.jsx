28948569ae9d7e6a58ee0040806c0d837bb5afef
turtore <brunosap@gmail.com>
2021-07-22 18:25:13 -0300

    atendida estrutura basica

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,1 +1,23 @@
 // implement MovieCard component here
+import React from 'react';
+// import movies from '../data';
+import MovieList from './MovieList';
+
+class MovieCard extends React.Component {
+  render() {
+    const { movie } = this.props;
+
+    return (
+      <section>
+        <h1>
+          Filme:
+          {movie.title}
+        </h1>
+        {/* <h2>teste</h2> */}
+
+      </section>
+    );
+  }
+}
+
+export default MovieCard;
