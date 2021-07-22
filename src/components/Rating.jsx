f77d7e5096ce47d7c3306b75136ebb1286796203
Matheus Camillo <matheuscamillo@hotmail.com.br>
2021-07-22 17:28:20 -0300

    projeto concluido

--- src/components/Rating.jsx
+++ src/components/Rating.jsx
@@ -1,1 +1,27 @@
-// implement Rating component here
+import React from 'react';
+import PropTypes from 'prop-types';
+
+class Rating extends React.Component {
+  render() {
+    const { rating } = this.props;
+    return (
+      <p className="rating">{ rating }</p>
+    );
+  }
+}
+
+Rating.propTypes = {
+  rating: PropTypes.arrayOf(PropTypes.shape({
+    title: PropTypes.string,
+    subtitle: PropTypes.string,
+    storyline: PropTypes.string,
+    rating: PropTypes.string,
+    imagePath: PropTypes.string,
+  })),
+};
+
+Rating.defaultProps = {
+  rating: [],
+};
+
+export default Rating;
