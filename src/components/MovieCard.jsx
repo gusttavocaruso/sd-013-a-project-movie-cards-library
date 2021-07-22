11f1fd8710409b1e6f1090cccec67a450a1bba2b
pedroalles <pedroalles@gmail.com>
2021-07-22 17:27:46 -0300

    Realizados requisitos do 11 ao 16

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import Rating from './Rating';
 
 export default class MovieCard extends Component {
   render() {
     const { movie } = this.props;
-    const { title, subtitle, storyline, imagePath } = movie;
+    const { title, subtitle, storyline, imagePath, rating } = movie;
     return (
       <section className="movie-card">
         <div className="movie-card-body">
@@ -13,9 +14,7 @@ export default class MovieCard extends Component {
           <h5 className="movie-card-subtitle">{ subtitle }</h5>
           <p className="movie-card-storyline">{ storyline }</p>
         </div>
-        <div className="movie-card-rating">
-          {/* <p>{rating}</p> */}
-        </div>
+        <Rating rating={ rating } />
       </section>
     );
   }
