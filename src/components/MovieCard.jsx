6d6a3418618659e8bfdece48e572769b3d39f389
Heleny Flavia <flaviaanahmias@gmail.com>
2021-07-22 18:25:59 -0400

    Corrige erros lint. AGORA VAI!

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,4 +1,4 @@
-// feito com a ajuda das mulheres da Turma 13 A - Jaqueline Silva, Vanessa Rios, Debora Teodorico, Isabella Antunes, Juliane Marques, Julia Baptista, Aline Eiko Hoshino, Elaine Moreira, Luiza Antiques, Sthefany Caroline, Jessica Queiroz, Lanai Conceicao e tambem Pedro Delicoli. 
+// feito com a ajuda das mulheres da Turma 13 A - Jaqueline Silva, Vanessa Rios, Debora Teodorico, Isabella Antunes, Juliane Marques, Julia Baptista, Aline Eiko Hoshino, Elaine Moreira, Luiza Antiques, Sthefany Caroline, Jessica Queiroz, Lanai Conceicao e tambem Pedro Delicoli.
 import React from 'react';
 import PropTypes from 'prop-types';
 import Rating from './Rating';
@@ -10,10 +10,10 @@ class MovieCard extends React.Component {
     return (
       <section className="movie-card">
         <img className="movie-card-image" src={ imagePath } alt={ title } />
-        <h4 className="movie-card-title" >{ title }</h4>
-        <h5 className="movie-card-subtitle" >{ subtitle }</h5>
-        <p className="movie-card-storyline" >{ storyline }</p>
-        <Rating rating = { rating } />
+        <h4 className="movie-card-title">{ title }</h4>
+        <h5 className="movie-card-subtitle">{ subtitle }</h5>
+        <p className="movie-card-storyline">{ storyline }</p>
+        <Rating rating={ rating } />
       </section>
     );
   }
