38619986184e547fcf755cddcca725f7d54f1751
Murilo Ribeiro <murilorsv14@gmail.com>
2021-07-22 18:27:56 -0300

    projetocards

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,1 +1,23 @@
 // implement MovieList component here
+import React from 'react';
+import PropsTypes from 'prop-types';
+import MovieCard from './MovieCard';
+
+class MovieList extends React.Component {
+  render() {
+    const { movies } = this.props;
+    const retorno = movies
+      .map((movie) => <MovieCard movie={ movie } key={ movie.title } />);
+    return (
+      <section className="movie-list">
+        {retorno}
+      </section>
+    );
+  }
+}
+
+MovieList.propTypes = {
+  movies: PropsTypes.arrayOf(PropsTypes.object).isRequired,
+};
+
+export default MovieList;
--- src/components/Rating.jsx
+++ src/components/Rating.jsx
@@ -1,1 +1,24 @@
 // implement Rating component here
+import React from 'react';
+import PropsTypes from 'prop-types';
+
+class Rating extends React.Component {
+  render() {
+    const { rating } = this.props;
+    return (
+      <p className="rating">
+        { rating }
+      </p>
+    );
+  }
+}
+
+Rating.propTypes = {
+  rating: PropsTypes.number,
+};
+
+Rating.defaultProps = {
+  rating: 0,
+};
+
+export default Rating;
