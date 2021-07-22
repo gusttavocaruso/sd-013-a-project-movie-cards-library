6d263e0e75693301c67a5a656751cb731db5121e
Joao Santos <jvitor.spaula@gmail.com>
2021-07-22 14:46:42 -0300

    Requisitos 1, 2, 3, 5

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,1 +1,11 @@
 // implement MovieList component here
+import React from 'react';
+// import MovieCard from './MovieCard';
+
+class MovieList extends React.Component {
+  render() {
+    return 0; // <MovieCard key={ MovieCard.id } />;
+  }
+}
+
+export default MovieList;
