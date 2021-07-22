b33119d823f26b92f5135c88505fdbfd31a247e3
Debora Teodorico <debora.steodorico@gmail.com>
2021-07-22 18:47:35 -0300

    Adicionando questao 11, 12, 13, 14, 15, 16, 17

--- src/components/Rating.jsx
+++ src/components/Rating.jsx
@@ -2,10 +2,15 @@ import React from 'react';
 
 class Rating extends React.Component {
   render() {
+    const { rating } = this.props;
     return (
-      <h1>test</h1>
+      <p className="rating">{ rating }</p>
     );
   }
 }
 
 export default Rating;
+
+Rating.propTypes = {
+  rating: propTypes.number.isRequired,
+};
