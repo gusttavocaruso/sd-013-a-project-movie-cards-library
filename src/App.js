3c0bb1550d804cbbc6bb767a761142fb975d9f24
Gabriel Priss <gabrielpriss@hotmail.com>
2021-07-22 17:18:07 -0300

    requisito 1

--- src/App.js
+++ src/App.js
@@ -1,14 +1,13 @@
 import React from 'react';
 import './App.css';
-import './Header'
-import MeuComponente from './Header';
+import Header from './components/Header';
 
 class App extends React.Component {
   render() {
     return (
       <div className="App">
         {<header>
-          <MeuComponente ></MeuComponente>
+          <Header />
         </header>}
       </div>
     );
