73678314ef08435737296b2741276fb2e4681786
Luiza Antiques <luiza.antiques@gmail.com>
2021-07-22 16:28:21 -0300

    Conclui requisitos 6, 7, 8 e 9

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -3,14 +3,14 @@ import PropType from 'prop-types';
 
 class MovieCard extends React.Component {
   render() {
-    const { movieInfo } = this.props;
-    const { imagePath, title, subtitle, storyline, rating } = movieInfo;
+    const { movie } = this.props;
+    const { imagePath, title, subtitle, storyline, rating } = movie;
 
     return (
       <div>
         <img src={ imagePath } alt={ title } />
-        <h3>{ title }</h3>
-        <h4>{ subtitle }</h4>
+        <h4>{ title }</h4>
+        <h5>{ subtitle }</h5>
         <p>{ storyline }</p>
         <p>{ rating }</p>
       </div>
@@ -19,7 +19,7 @@ class MovieCard extends React.Component {
 }
 
 MovieCard.propTypes = {
-  movieInfo: PropType.shape({
+  movie: PropType.shape({
     title: PropType.string,
     subtitle: PropType.string,
     storyline: PropType.string,
--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -8,7 +8,7 @@ class MovieList extends React.Component {
 
     return (
       movies.map((movie) => (
-        <MovieCard key={ movie.title } movieInfo={ movie } />))
+        <MovieCard key={ movie.title } movie={ movie } />))
     );
   }
 }
