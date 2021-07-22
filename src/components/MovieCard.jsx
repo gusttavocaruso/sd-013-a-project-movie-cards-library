77437c87d29439a9e53f5b95fd2a4e0e232e1957
Gean634N <gean.a.l.c@gmail.com>
2021-07-22 15:58:29 -0400

    Renderiza imagem de filme unica

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,11 +1,24 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import Rating from './Rating';
 
 class MovieCard extends React.Component {
   render() {
+    const { movie } = this.props;
     return (
-      <p>Movie</p>
+      <div>
+        <img src={ movie.imagePath } alt="" />
+        <h4>Titulo</h4>
+        <h5>Subtitulo</h5>
+        <p>Sinopse</p>
+        <Rating />
+      </div>
     );
   }
 }
+
+MovieCard.propTypes = {
+  movie: PropTypes.isRequired,
+};
 
 export default MovieCard;
--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,13 +1,18 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import MovieCard from './MovieCard';
 
 class MovieList extends React.Component {
   render() {
-    // const { movies } = this.props;
+    const { movies } = this.props;
     return (
-      <MovieCard />
+      <MovieCard movie={ movies[0] } />
     );
   }
 }
+
+MovieList.propTypes = {
+  movies: PropTypes.arrayOf(PropTypes.object).isRequired,
+};
 
 export default MovieList;
