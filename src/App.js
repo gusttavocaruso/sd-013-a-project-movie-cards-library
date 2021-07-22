365b69fa2de2dfc821651228156b0613c6eab523
deyvid-dev <deyvidscm@hotmail.com>
2021-07-22 15:59:09 -0300

    requisito 2

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import MovieList from './components/MovieList';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <React />
+      <MovieList />
       Hora de codar! 🚀✍️👨‍💻👩‍💻💪
     </div>
   );
