cbf9f17e876d673fbd5c0b0591d30dedc7df83ca
Pedro Verano <pedroverano2009@yahoo.com.br>
2021-07-22 15:59:09 -0300

    Adiciona o requisito 1

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,8 +4,8 @@ class Header extends React.Component {
   render() {
     return (
       <header>
-        <h1>Movie Cards</h1>
-    </header>
+        <h1>Movie Cards Library</h1>
+      </header>
     );
   }
 }
