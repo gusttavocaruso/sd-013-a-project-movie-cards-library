d2a11747c705033b0d4ed694c7a2be400f471636
Jéssica Queiroz <gnr0034@gmail.com>
2021-07-22 16:18:08 -0300

    Requisito 3

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,1 +1,13 @@
 // implement MovieList component here
+import React from 'react';
+import movies from '../data';
+
+class MovieList extends React.Component {
+  render() {
+    return (
+      <div>{movies}</div>
+    );
+  }
+}
+
+export default MovieList;
