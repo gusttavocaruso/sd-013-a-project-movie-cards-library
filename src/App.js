47548043f9a84e0fdec713aabe0b227cd31bd1ed
Joao Vieira <jonnyvieirausa@hotmail.com>
2021-07-22 14:48:02 -0300

    iniciando o projeto. VAMOS COM TUDO :rocket:

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './App.css';
 
-function App() {
+function App(){
   return (
     <div className="App">
       {/* Sua implementação deve ficar aqui. Remova essas duas linhas e mão na massa */}
