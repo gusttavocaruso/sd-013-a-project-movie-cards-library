9597971826e348828b9e973659de18decaa797a6
Gabriel Biasoli <bielbiasoli33@icloud.com>
2021-07-22 15:05:59 -0300

    Finaliza requisito 3

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,1 +1,13 @@
-// implement MovieCard component here
+import React from 'react';
+
+class MovieCard extends React.Component {
+  render() {
+    return (
+      <section>
+        <h1>Hye</h1>
+      </section>
+    );
+  }
+}
+
+export default MovieCard;
