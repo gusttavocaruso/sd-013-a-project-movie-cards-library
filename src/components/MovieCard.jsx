855b4ef2831065c897a5e0911001662928c30431
Gustavo Mauricio <gmauricio207@gmail.com>
2021-07-22 15:08:45 -0300

    Req 3 e 4 Realizado

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,1 +1,17 @@
 // implement MovieCard component here
+import React, { Component } from 'react';
+
+class MovieCard extends Component {
+    render() {
+        const { movie: { title, subtitle, storyline, rating, imagePath }} = this.props.
+        return(
+            <div>
+                <div>
+
+                </div>
+            </div>
+        )
+    }
+}
+
+export default MovieCard;
--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,11 +1,17 @@
 // implement MovieList component here
 import React, { Component } from 'react';
+import MovieCards from './MovieCard'
+
 
 class MovieList extends Component {
     render() {
+        const { movies } = this.props;
+
         return(
-            
-        )
+            <div>
+              {movies.map(movie => <MovieCards key={movie.title} movie={movie} />)}  
+            </div>
+        );
     }
 }
 export default MovieList;
