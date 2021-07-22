0c548cf5dc4805b358cbfc749d16e03ef0d08609
Luiza Antiques <luiza.antiques@gmail.com>
2021-07-22 17:02:39 -0300

    Conclui Requisitos 12, 13 e 14

--- src/components/Rating.jsx
+++ src/components/Rating.jsx
@@ -1,8 +1,18 @@
 import React from 'react';
+import PropType from 'prop-types';
 
 class Rating extends React.Component {
   render() {
+    const { rating } = this.props;
+
+    return (
+      <p className="rating">{ rating }</p>
+    );
   }
 }
+
+Rating.propTypes = {
+  rating: PropType.number.isRequired,
+};
 
 export default Rating;
