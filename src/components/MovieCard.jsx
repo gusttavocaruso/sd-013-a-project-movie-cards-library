561ac1988ce9fe59dcf294a1c83912f06b910896
V. Dionysio <vini.dionysio@gmail.com>
2021-07-22 17:15:07 -0300

    reqs 12, 13 and 14

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,6 +1,7 @@
 // implement MovieCard component here
-import PropTypes from 'prop-types';
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import Rating from './Rating';
 
 class MovieCard extends Component {
   render() {
@@ -8,10 +9,12 @@ class MovieCard extends Component {
     const { movie: { title, subtitle, storyline, rating, imagePath } } = this.props;
     return (
       <div>
-        <h4>{title}</h4>
-        <h5>{subtitle}</h5>
-        <p>{storyline}</p>
-        {rating}
+        <div>
+          <h4>{title}</h4>
+          <h5>{subtitle}</h5>
+          <p>{storyline}</p>
+          <Rating rating={ rating } />
+        </div>
         <img src={ imagePath } alt="" />
       </div>
     );
