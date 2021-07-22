367883fa4b247f82b12b541df34d19ba8a7e3d62
Juliane Marques <mqs.juliane@gmail.com>
2021-07-22 18:41:42 -0300

    adiciona style

--- src/components/Rating.jsx
+++ src/components/Rating.jsx
@@ -1,4 +1,6 @@
-// implement Rating component here
+/* feito com a ajuda das mulheres da Turma 13 A - Jaqueline Silva, Vanessa Rios, Debora Teodorico, Isabella Antunes, Heleny Flavia, Julia Baptista, Aline Eiko Hoshino, Elaine Moreira, Luiza Antiques, Sthefany Caroline, Jessica Queiroz, Lanai Conceicao e tambem Pedro Delicoli.
+https://reactjs.org/docs/typechecking-with-proptypes.html#default-prop-values
+implement MovieCard component here */
 import React from 'react';
 import PropTypes from 'prop-types';
 
