fc04068315256a9fbe3988093ba844a4db9b9303
Breno Lopes <brenokos4@gmail.com>
2021-07-22 19:26:08 -0300

    add competencia 12, 13 e 14!

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,15 +1,21 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import Rating from './Rating';
 
 class MovieCard extends React.Component {
   render() {
     const { title, subtitle, storyline, imagePath } = this.props.movie;
     return (
       <div>
-        <h4>{ title }</h4>
-        <h5>{ subtitle }</h5>
-        <p>{ storyline }</p>
-        <img src={ imagePath } alt={ title } />
+        <div>
+          <h4>{ title }</h4>
+          <h5>{ subtitle }</h5>
+          <p>{ storyline }</p>
+          <img src={ imagePath } alt={ title } />
+        </div>
+        <div>
+          <Rating />
+        </div>
       </div>
     );
   }
