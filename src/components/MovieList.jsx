250d96748971a9a9808de34711a4d6f496669acb
sonkai <mfilho86@gmail.com>
2021-07-22 17:38:38 -0300

    requisito 4, mas o lint pedia o 5 e o seis foi junto

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,11 +1,20 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import MovieCard from './MovieCard';
 
 class MovieList extends React.Component {
   render() {
+    const { movies } = this.props;
     return (
-      <div>oi!</div>
+      <div>
+        { movies.map((movie) => <MovieCard key={ movie.title } movie={ movie } />) }
+      </div>
     );
   }
 }
+
+MovieList.propTypes = {
+  movies: PropTypes.shape([]).isRequired,
+};
 
 export default MovieList;
