d7e4e619c1d3539f3ee2a86644d24bbde4056b3d
Reinaldo Paixao <reipaixao.b@gmail.com>
2021-07-22 19:27:54 -0300

    Adiciona ate requisito 10

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -3,11 +3,16 @@ import React from 'react';
 class MovieCard extends React.Component {
   render() {
     const { movie } = this.props;
-    const { title, subtitle, storyline, imagePath, rating } = movie;
-
     return (
+      <img
+        key="image"
+        className="movie-card-image"
+        src={ movie.imagePath }
+        alt={ movie.title }/>,
       <div>
-        <p> Texto </p>
+        <h4 key="title" className="movie-card-title">{movie.title}</h4>
+        <h5 key="subtitle" className="movie-card-subtitle">{movie.subtitle}</h5>
+        <p key="synopsis" className="movie-card-storyline">{movie.storyline}</p>
       </div>
     );
   }
