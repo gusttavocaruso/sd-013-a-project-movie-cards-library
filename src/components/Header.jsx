ae6bd6fbd7a6123868b5d3b0f2f5a8754c2a5529
MatheusXI <83838699+MatheusXI@users.noreply.github.com>
2021-07-22 19:18:37 -0300

    Update Header.jsx

---
+++ src/components/Header.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class Header extends Component {
+  render() {
+    return (
+      <header>
+        <h1>Movie Cards Library</h1>
+      </header>
+      // Lint chatão //
+    );
+  }
+}
+
+export default Header;
