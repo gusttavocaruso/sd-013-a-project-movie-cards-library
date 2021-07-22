7ee6a48d0facc9045fabfd5aa05c086ca204328e
Juliane Marques <mqs.juliane@gmail.com>
2021-07-22 16:55:51 -0300

    testando requisito 4

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -5,9 +5,10 @@ import PropTypes from 'prop-types';
 class MovieList extends React.Component {
   render() {
     const { movies } = this.props;
-    console.log(movies);
     return (
-      <p>teste</p>
+      <div>
+          {movies.map((movie) => <MovieCard key={ movie.tile } />)}
+      </div>
     );
   }
 }
