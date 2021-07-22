d0e636cdf0bfd30c723d08566b47cee8c24104d4
Vanessa <vanessamrios@gmail.com>
2021-07-22 16:32:50 -0300

    Atende aos requisitos 3 e 16

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,6 +1,19 @@
 // implement MovieList component here
-// import React from 'react';
+import React from 'react';
+import PropTypes from 'prop-types';
 
-// class MovieList extends React.Component {
+class MovieList extends React.Component {
+  render() {
+    const { movies } = this.props;
+    console.log(movies);
+    return (
+      <p>tese</p>
+    );
+  }
+}
 
-// }
+MovieList.propTypes = {
+  movies: PropTypes.arrayOf(PropTypes.object).isRequired,
+};
+
+export default MovieList;
