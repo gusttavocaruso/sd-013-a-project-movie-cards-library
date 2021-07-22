0f795c5f002041f7255cde7c2863d48b475f67ff
Murilo Rainho <murilorainho01@gmail.com>
2021-07-22 14:38:04 -0300

    [Murilo-Rainho] Movie Cards Library Project

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,1 +1,35 @@
-// implement MovieCard component here
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import Rating from './Rating';
+
+class MovieCard extends Component {
+  render() {
+    const {
+      imagePath,
+      title,
+      subtitle,
+      storyline,
+      rating } = this.props;
+
+    return (
+      <section>
+        <img src={ imagePath } alt={ title } />
+        <h4>{title}</h4>
+        <h5>{subtitle}</h5>
+        <p>{storyline}</p>
+        {/* <h2>{this.props.rating}</h2> */}
+        <Rating rating={ rating } />
+      </section>
+    );
+  }
+}
+
+MovieCard.propTypes = {
+  imagePath: PropTypes.string.isRequired,
+  title: PropTypes.string.isRequired,
+  subtitle: PropTypes.string.isRequired,
+  storyline: PropTypes.string.isRequired,
+  rating: PropTypes.number.isRequired,
+};
+
+export default MovieCard;
--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,1 +1,25 @@
-// implement MovieList component here
+import React, { Component } from 'react';
+import movies from '../data';
+import MovieCard from './MovieCard';
+
+class MovieList extends Component {
+  render() {
+    const cards = movies.map((card, index) => (
+      <MovieCard
+        key={ index }
+        imagePath={ card.imagePath }
+        title={ card.title }
+        subtitle={ card.subtitle }
+        storyline={ card.storyline }
+        rating={ card.rating }
+      />
+    ));
+    return (
+      <div className="movieList__container">
+        { cards }
+      </div>
+    );
+  }
+}
+
+export default MovieList;
