9f609a1a812679eee1f10d98f905858d9b6a1e0a
Rafhael Gomes <rafhael.andrade@hotmail.com.br>
2021-07-22 18:22:50 -0300

    finalizado

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import './App.css';
 import Header from './components/Header';
 import MovieList from './components/MovieList';
+import movies from './data';
 
 function App() {
+  const moviesMap = movies.map((movie) => movie);
   return (
     <div className="App">
       <Header />
-      <MovieList />
+      <MovieList movies={ moviesMap } />
     </div>
   );
 }
--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import MovieCard from './MovieCard';
-import movies from '../data';
 
 export default class MovieList extends React.Component {
   render() {
+    const { movies } = this.props;
     const moviesMap = movies
       .map((obj) => (<MovieCard
         movie={ obj }
@@ -16,3 +17,7 @@ export default class MovieList extends React.Component {
       </main>);
   }
 }
+
+MovieList.propTypes = {
+  movies: PropTypes.arrayOf(PropTypes.object).isRequired,
+};
