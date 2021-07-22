bf3a872a0a54ac9c0a673e2a20b6d4457f2774b4
Gean634N <gean.a.l.c@gmail.com>
2021-07-22 14:27:36 -0400

    :package: Requisito 1

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,11 @@
-ogustavomauricio
-// implement Header component here
+import React from 'react';
 
- master
+class Header extends React.Component {
+  render() {
+    return (
+      <h1>Movie Cards Library</h1>
+    );
+  }
+}
+
+export default Header;
