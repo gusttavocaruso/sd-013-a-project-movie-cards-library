6d9c470cc959fcf28298126b379b87f348f83f62
Lanai Conceição <conceicao.lanai@gmail.com>
2021-07-22 18:31:52 -0300

    Requisito 11

--- src/components/Rating.jsx
+++ src/components/Rating.jsx
@@ -1,1 +1,13 @@
-// implement Rating component here
+import React from 'react';
+
+class Rating extends React.Component {
+  render() {
+    return (
+      <div>
+        <p>teste 3</p>
+      </div>
+    );
+  }
+}
+
+export default Rating;
