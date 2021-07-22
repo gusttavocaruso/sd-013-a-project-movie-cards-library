fdbe824f15c5339dc2016d5a642346bc7e127d83
V. Dionysio <vini.dionysio@gmail.com>
2021-07-22 16:00:41 -0300

    reqs 3 and 16

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,1 +1,16 @@
 // implement MovieList component here
+import React, { Component } from 'react';
+
+class MovieList extends Component {
+  render() {
+    const { movies } = this.props;
+
+    return (
+      <div>
+        {movies.map((movie) => <h1>movie</h1>)}
+      </div>
+    );
+  }
+}
+
+export default MovieList;
