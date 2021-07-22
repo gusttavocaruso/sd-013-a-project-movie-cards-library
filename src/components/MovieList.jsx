0050c7574e074b464e3cabe70bef4bb4cf80bcf8
Rafael Victor <rafavictorgs@gmail.com>
2021-07-22 17:32:27 -0300

    Corrigindo erros lint

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,1 +1,11 @@
-// implement MovieList component here
+import React from 'react';
+
+class MovieList extends React.Component {
+  render() {
+    return (
+      <p>Oi!</p>
+    );
+  }
+}
+
+export default MovieList;
