840139f3e90d1e1782a43958001a2304897a2838
Fabiola Moutinho <fabiolamoutinho@hotmail.com>
2021-07-22 15:46:19 -0400

    Requisito 1

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,1 +1,13 @@
-// começando
+import React from 'react';
+
+class Header extends React.Component {
+  render() {
+    return (
+      <header className="movie-card-header">
+        
+      </header>
+    );
+  }
+}
+
+export default Header;
