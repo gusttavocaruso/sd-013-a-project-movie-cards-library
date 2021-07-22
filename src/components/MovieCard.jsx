3856b43dcc2418e08e1b526ae4e9d44a2d354c75
Erick Santos <erickosantos.dev@gmail.com>
2021-07-22 16:06:30 -0300

    9 - Renderize o subtítulo do filme dentro de uma tag h5

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -4,10 +4,11 @@ import PropTypes from 'prop-types';
 class MovieCard extends React.Component {
   render() {
     const { movie } = this.props;
-    const { title, imagePath } = movie;
+    const { title, imagePath, subtitle } = movie;
     return (
       <div>
         <h4>{ title }</h4>
+        <h5>{ subtitle }</h5>
         <img src={ imagePath } alt={ title } />
       </div>
     );
@@ -17,6 +18,7 @@ MovieCard.propTypes = {
   movie: PropTypes.shape({
     title: PropTypes.string,
     imagePath: PropTypes.string,
+    subtitle: PropTypes.string,
   }).isRequired,
 };
 
