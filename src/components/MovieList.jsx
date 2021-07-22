355b720021296180e756574e58290c4be729c549
Fabiola Moutinho <fabiolamoutinho@hotmail.com>
2021-07-22 16:27:55 -0400

    Requisito 3

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,1 +1,11 @@
-// implement MovieList component here
+import React from 'react';
+
+class MovieList extends React.Component {
+  render() {
+    return (
+      <h1>Vamo que vamo</h1>
+    );
+  }
+}
+
+export default MovieList;
