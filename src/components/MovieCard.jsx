537809528413b43b0bf5a9cf4ec82497831ebce1
marcos <marc0sdurval20@gmail.com>
2021-07-22 15:38:58 -0300

    1 e 2

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,1 +1,15 @@
 // implement MovieCard component here
+import React from 'react';
+
+class MovieCard extends React.Component {
+  render() {
+    const { filme } = this.props;
+    return (
+      <div>
+        <img src={ filme.imagePath } alt="" />
+      </div>
+    );
+  }
+}
+
+export default MovieCard;
--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,1 +1,16 @@
 // implement MovieList component here
+import React from 'react';
+import MovieCard from './MovieCard';
+import movies from '../data';
+
+class MovieList extends React.Component {
+  render() {
+    return (
+      <div>
+        {movies.map((elemnt, index) => <MovieCard key={ index } filme={ elemnt } />)}
+      </div>
+    );
+  }
+}
+
+export default MovieList;
