256be042caf27de521822b8cef0380df51b12f81
FelipeNevess <felipe012neves@gmail.com>
2021-07-22 20:50:56 -0300

    Projeto finalizado sem ProtTypes

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,1 +1,21 @@
 // implement MovieCard component here
+import React, { Component } from 'react';
+import Rating from './Rating';
+
+class MovieCard extends Component {
+  render() {
+    const { movie } = this.props;
+    const { title, subtitle, rating, storyline, imagePath } = movie;
+    return (
+      <div>
+        <img src={ imagePath } alt={ title } />
+        <h4>{ title }</h4>
+        <h5>{ subtitle }</h5>
+        <p>{ storyline }</p>
+        <Rating rating={ rating } />
+      </div>
+    );
+  }
+}
+
+export default MovieCard;
