33a7cd34f206c72a259cc1b303a985f4d899b6bb
thomasravache <thomasravache31@gmail.com>
2021-07-22 18:59:01 -0300

    Requisitos 4 e 5 - Com correçoes de lint

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,21 +1,43 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 class MovieCard extends React.Component {
   render() {
-    const { title, subtitle, storyline, rating, imagePath } = this.props.dataMovie;
-    return(
+    const { dataMovie: { title, subtitle, storyline, rating, imagePath } } = this.props;
+    return (
       <div>
         <div>
-          <img src={imagePath} alt={title} />
+          <img src={ imagePath } alt={ title } />
         </div>
         <div>
-          <h3>{title}</h3>
-          <h4>{subtitle}</h4>
-          <p>{storyline}</p>
+          <h3>{ title }</h3>
+          <h4>{ subtitle }</h4>
+          <p>{ storyline }</p>
+          <p>{ rating }</p>
         </div>
       </div>
     );
   }
 }
+
+MovieCard.propTypes = {
+  dataMovie: PropTypes.shape({
+    title: PropTypes.string,
+    subtitle: PropTypes.string,
+    storyline: PropTypes.string,
+    rating: PropTypes.number,
+    imagePath: PropTypes.string,
+  }),
+};
+
+MovieCard.defaultProps = {
+  dataMovie: {
+    title: 'Filmezinho',
+    subtitle: 'Filmezinho top',
+    storyline: 'filminho bom demais',
+    rating: 5,
+    imagePath: 'https://pics.me.me/steven-seagal-steven-seagull-steven-seagull-19223227.png',
+  },
+};
 
 export default MovieCard;
--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
-import MovieCard from './MovieCard'
+import PropTypes from 'prop-types';
+import MovieCard from './MovieCard';
 
 class MovieList extends React.Component {
   render() {
@@ -7,10 +8,28 @@ class MovieList extends React.Component {
 
     return (
       <section>
-        { movies.map((movie) => <MovieCard dataMovie={ movie } />) }
+        { movies.map((movie) => <MovieCard key={ movie.title } dataMovie={ movie } />) }
       </section>
     );
   }
 }
+
+MovieList.propTypes = {
+  movies: PropTypes.arrayOf(
+    PropTypes.object.isRequired,
+  ),
+};
+
+MovieList.defaultProps = {
+  movies: [
+    {
+      title: 'teste',
+      subtitle: 'teste',
+      storyline: 'Steven Seagall',
+      rating: 4.5,
+      imagePath: 'https://pics.me.me/steven-seagal-steven-seagull-steven-seagull-19223227.png',
+    },
+  ],
+};
 
 export default MovieList;
