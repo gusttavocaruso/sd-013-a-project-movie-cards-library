0711f8c628229b2ab83b3032a107a6cfb7220284
Fernando Costa <fgosuendacosta@gmail.com>
2021-07-22 14:21:00 -0300

    Realizando primeiros requisitos 8 a 10

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,1 +1,29 @@
-// implement MovieList component here
+// Crie um componente que represente toda a área com os cartões de filmes
+import React from 'react';
+import PropTypes from 'prop-types';
+import MovieCard from './MovieCard';
+
+class MovieList extends React.Component {
+  render() {
+    // Recebo a props "movies" do App.js que é um objeto do array de movies
+    // Nesses objetos temos a informação de title
+    const { movies: { title } } = this.props; // Desestruturo para fazer a chamada do title
+
+    // Renderizo abaixo a props movies enviada pelo App.js que foi desestruturada
+    return (
+      <section className="movie-list">
+        {/* Toda vez que é realizado o MAP é necessário colocar um KEY, usado internamente para identificar cada item */}
+        { movies.map((movie) => <MovieCard key={ title } movie={ movie } />)}
+      </section>
+    );
+  }
+}
+
+// Eu espero receber uma prop do title e tem que ser string
+MovieList.propTypes = { // Iniciando a checagem de tipos das Props
+  movies: PropTypes.shape({
+    title: PropTypes.string, // Eu espero receber uma prop do title e tem que ser number
+  }).isRequired, // Para torna-los obrigatório
+};
+
+export default MovieList; // Sempre exportar o componente
