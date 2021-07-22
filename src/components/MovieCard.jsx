7b9a4bfaaac878a533fae636abea55dc54ce4751
Julia Baptista <baptista.julia@yahoo.com.br>
2021-07-22 17:35:07 -0300

    Quesito 7

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -3,9 +3,11 @@ import React from 'react';
 
 class MovieCard extends React.Component {
   render() {
+    const { movie } = this.props;
+    const { title, subtitle, storyline, rating, imagePath } = movie;
     return (
       <section>
-        <p>teste</p>
+        <img src={ imagePath } alt={ title } />
       </section>
     );
   }
