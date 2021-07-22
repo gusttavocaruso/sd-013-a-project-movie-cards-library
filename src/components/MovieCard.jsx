26e5556723f8f79806d898a31a1d1b18bce07ad0
Matheus Duarte de Freitas <freickup@gmail.com>
2021-07-22 16:21:34 -0300

    resolucao ate a 16

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,1 +1,21 @@
 // implement MovieCard component here
+import React from 'react';
+import Rating from './Rating';
+
+class MovieCard extends React.Component {
+  render() {
+    const { movie } = this.props;
+    const { title, imagePath, subtitle, storyline, rating } = movie;
+    return (
+      <section className="movie-card">
+        <img src={ imagePath } alt={ title } />
+        <h4>{ title }</h4>
+        <h5>{ subtitle }</h5>
+        <p>{ storyline }</p>
+        <Rating rating={ rating } />
+      </section>
+    );
+  }
+}
+
+export default MovieCard;
--- src/components/Rating.jsx
+++ src/components/Rating.jsx
@@ -1,1 +1,16 @@
 // implement Rating component here
+import React from 'react';
+
+class Rating extends React.Component {
+  render() {
+    const { rating } = this.props;
+    // const { title, imagePath, subtitle, storyline, rating } = movieData;
+    return (
+      <section>
+        <p className="rating">{ rating }</p>
+      </section>
+    );
+  }
+}
+
+export default Rating;
