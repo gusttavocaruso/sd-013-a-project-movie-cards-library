1db98d424354540e41d75e9aea48233928012698
Samuel Guilherme <samuel.guilherme.l@hotmail.com>
2021-07-22 20:19:49 -0300

    arrumando o errro Parse errors in imported module do lint

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -9,7 +9,7 @@ class MovieCard extends React.Component {
       <section>
         <img src={ imagePath } alt={ title } />
         <h4 className=".movie-card-title">{ title }</h4>
-        <h5>{ subtitle }</h5>
+        <h5 className=".movie-card-subtitle">{ subtitle }</h5>
         <p>{ storyline }</p>
         <Rating rating={ rating } />
       </section>
