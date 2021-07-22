d83d628d39f21455e9eb1e11508dc99b11b75368
Gean634N <gean.a.l.c@gmail.com>
2021-07-22 16:13:41 -0400

    :package: Requisito 12

--- src/components/Rating.jsx
+++ src/components/Rating.jsx
@@ -5,7 +5,7 @@ class Rating extends React.Component {
   render() {
     const { rating } = this.props;
     return (
-      <rating>{ rating }</rating>
+      <rating className="rating">{ rating }</rating>
     );
   }
 }
