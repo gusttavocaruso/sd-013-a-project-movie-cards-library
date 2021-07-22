de3c257a6c70ba86ecd46e576e12a919a5033f78
Icaro Ferreira <icaro.iky@gmail.com>
2021-07-22 15:46:30 -0300

    Req 3 e 4

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,1 +1,11 @@
-// implement MovieCard component here
+import React from 'react';
+
+class MovieCard extends React.Component {
+  render() {
+    return (
+      <div>Filme 1</div>
+    );
+  }
+}
+
+export default MovieCard;
