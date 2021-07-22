e811432e0ec4e5fa6a2a9c938d88c3bfb955a289
gustavo.caruso <gustavoalmeida26@hotmail.com>
2021-07-22 19:27:03 -0300

    add CSS style to the project

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -9,13 +9,13 @@ class MovieCard extends React.Component {
     } = this.props;
 
     return (
-      <div>
-        <section>
-          <h4>{ title }</h4>
-          <h5>{ subtitle }</h5>
-          <p>{ storyline }</p>
-          <img src={ imagePath } alt={ title } />
-          <Rating rating={ rating } />
+      <div className="movie-card">
+        <section className="movie-card-body">
+          <h4 className="movie-card-title">{ title }</h4>
+          <h5 className="movie-card-subtitle">{ subtitle }</h5>
+          <p className="movie-card-storyline">{ storyline }</p>
+          <img className="movie-card-image" src={ imagePath } alt={ title } />
+          <Rating className="movie-card-rating" rating={ rating } />
         </section>
       </div>
     );
