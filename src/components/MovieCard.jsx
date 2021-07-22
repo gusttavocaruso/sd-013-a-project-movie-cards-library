7b8b6c832439a03f132e51a65ee589f5aa6eca63
Juliane Marques <mqs.juliane@gmail.com>
2021-07-22 17:37:02 -0300

    arruma lint 02

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -6,8 +6,8 @@ import React from 'react';
 
 class MovieCard extends React.Component {
   render() {
-  const { movie } = this.props;
-  const { title, subtitle, storyline, imagePath, rating } = movie;
+    const { movie } = this.props;
+    const { title, subtitle, storyline, imagePath, rating } = movie;
     return (
       <div>
         <img className="movie-card-image" src={ imagePath } alt={ title } />
