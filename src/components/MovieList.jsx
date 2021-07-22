49dc7eb733fa66606591fa063228efef75bedff4
PC <sthecso>
2021-07-22 17:06:18 -0300

    Resolver requisito 4 e 5

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,12 +1,12 @@
 import React, { Component } from 'react';
 import Proptypes from 'prop-types';
+import MovieCard from './MovieCard';
 
 class MovieList extends Component {
   render() {
     const { movies } = this.props;
-    console.log(movies);
     return (
-      <p />
+      movies.map((movie) => <MovieCard key={ movie.title } movie={ movie } />)
     );
   }
 }
