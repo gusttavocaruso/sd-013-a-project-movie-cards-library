b49f87af4829174e683e55c46cc44522c4f451e0
Gean634N <gean.a.l.c@gmail.com>
2021-07-22 16:08:42 -0400

    :up: renderiza um filme

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -5,13 +5,14 @@ import Rating from './Rating';
 class MovieCard extends React.Component {
   render() {
     const { movie } = this.props;
+    const { title, subtitle, storyline, imagePath, rating } = movie;
     return (
       <div>
-        <img src={ movie.imagePath } alt="" />
-        <h4>Titulo</h4>
-        <h5>Subtitulo</h5>
-        <p>Sinopse</p>
-        <Rating />
+        <img src={ imagePath } alt="" />
+        <h4>{ title }</h4>
+        <h5>{ subtitle }</h5>
+        <p>{ storyline }</p>
+        <Rating rating={ rating } />
       </div>
     );
   }
