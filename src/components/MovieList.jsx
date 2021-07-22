04c73cc4095428405688f5575ee9baa7871746c7
Heleny Flavia <flaviaanahmias@gmail.com>
2021-07-22 15:33:25 -0400

    Requisito 3

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,1 +1,19 @@
 // implement MovieList component here
+import React from 'react';
+import PropTypes from 'prop-types';
+
+class MovieList extends React.Component {
+  render() {
+    const { movies } = this.props;
+    console.log(movies);
+    return (
+      <p>Teste</p>
+    );
+  }
+}
+
+MovieList.propTypes = {
+  movies: PropTypes.arrayOf(PropTypes.object).isRequired,
+};
+
+export default MovieList;
