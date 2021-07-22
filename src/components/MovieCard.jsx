f22f43dbf477301a4b302367a696f2cbd83affbb
PC <sthecso>
2021-07-22 18:22:11 -0300

    Resolver requisitos 6 a 10

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,11 +1,29 @@
 import React, { Component } from 'react';
+import Proptypes from 'prop-types';
 
 class MovieCard extends Component {
   render() {
+    const { movie } = this.props;
+    const { title, subtitle, storyline, imagePath } = movie;
+
     return (
-      <div>test</div>
+      <div>
+        <img src={ imagePath } alt="Imagem do filme" />
+        <h4>{title}</h4>
+        <h5>{subtitle}</h5>
+        <p>{storyline}</p>
+      </div>
     );
   }
 }
+
+MovieCard.propTypes = {
+  movie: Proptypes.shape({
+    title: Proptypes.string,
+    subtitle: Proptypes.string,
+    storyline: Proptypes.string,
+    imagePath: Proptypes.string,
+  }).isRequired,
+};
 
 export default MovieCard;
