289535efac8983c473f726680156ed1dfb8638f3
deyvid-dev <deyvidscm@hotmail.com>
2021-07-22 19:12:30 -0300

    alguns requisitos

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -3,9 +3,15 @@ import React from 'react';
 
 class MovieCard extends React.Component {
   render() {
+    const { movie } = this.props;
+    const { title, subtitle, storyline, imagePath, rating } = movie;
+
     return (
       <div>
-        <p>ok</p>
+        <img src={ imagePath } alt={ title } />
+        <h4>{title}</h4>
+        <h5>{subtitle}</h5>
+        <p>{storyline}</p>
       </div>
     );
   }
