46993559a63c8c2448080fa4694fcfe70348df90
PC <sthecso>
2021-07-22 16:32:45 -0300

    Resolver requisito 3

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,1 +1,18 @@
-// implement MovieList component here
+import React, { Component } from 'react';
+import Proptypes from 'prop-types';
+
+class MovieList extends Component {
+  render() {
+    const { movies } = this.props;
+    console.log(movies);
+    return (
+      <p />
+    );
+  }
+}
+
+MovieList.propTypes = {
+  movies: Proptypes.arrayOf(Proptypes.object).isRequired,
+};
+
+export default MovieList;
