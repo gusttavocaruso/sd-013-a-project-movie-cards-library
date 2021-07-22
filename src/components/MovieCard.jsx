57568744241e50301f3b0cf5a394ffe83a5a02fc
Thiago <thiagoc.carboneri@gmail.com>
2021-07-22 18:51:38 -0300

    corrigindo lint

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -20,13 +20,13 @@ class MovieCard extends React.Component {
 }
 
 MovieCard.propTypes = {
-  movie:PropTypes.shape({
-      title: PropTypes.string,
-      subtitle: PropTypes.string,
-      storyline: PropTypes.string,
-      rating: PropTypes.number,
-      imagePath: PropTypes.string,
-    }).isRequired,
+  movie: PropTypes.shape({
+    title: PropTypes.string,
+    subtitle: PropTypes.string,
+    storyline: PropTypes.string,
+    rating: PropTypes.number,
+    imagePath: PropTypes.string,
+  }).isRequired,
 };
 
 export default MovieCard;
