0c3f449180b1d8cea20c68a072e5bf70f44ac620
Ingo Reichert Junior <ingoreichertjr@gmail.com>
2021-07-22 20:11:31 -0300

    Adiciona requisitos do 11 ao 16

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,15 +1,18 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import Rating from './Rating';
 
 class MovieCard extends React.Component {
   render() {
-    const { movie: { title, subtitle, storyline, imagePath } } = this.props;
+    const { movie: { title, subtitle, storyline, imagePath, rating } } = this.props;
     return (
       <div>
         <h4>{ title }</h4>
         <h5>{ subtitle }</h5>
         <p>{ storyline }</p>
         <img src={ imagePath } alt={ `${title}` } />
+        { rating }
+        <Rating rating={ rating } />
       </div>
     );
   }
@@ -21,6 +24,7 @@ MovieCard.propTypes = {
     subtitle: PropTypes.string,
     storyline: PropTypes.string,
     imagePath: PropTypes.string,
+    rating: PropTypes.number,
   }).isRequired,
 };
 
