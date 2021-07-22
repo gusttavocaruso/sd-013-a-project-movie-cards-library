905f09ab38ab0ed830a6edc25b54af25f08a194b
Julia Baptista <baptista.julia@yahoo.com.br>
2021-07-22 16:12:32 -0300

    Quesito 3

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,1 +1,14 @@
-// implement MovieList component here
+import React from 'react';
+
+class MovieList extends React.Component {
+  render() {
+    const { movies } = this.props;
+    return (
+      <header>
+        <h1>test</h1>
+      </header>
+    );
+  }
+}
+
+export default MovieList;
