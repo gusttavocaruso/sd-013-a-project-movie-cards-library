90bb4f32b5b5804cf33ab68ea5b1c5c64aa1a700
igor <igorhdsouza@gmail.com>
2021-07-22 19:07:07 -0300

    renderiza header e MovieList dentro do ./app

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,1 +1,11 @@
-// implement MovieList component here
+import React from 'react';
+
+class MovieList extends React.Component {
+  render() {
+    // eslint-disable-next-line react/prop-types
+    const { movies } = this.props;
+    console.log(movies);
+    return <h2>olá</h2>;
+  }
+}
+export default MovieList;
