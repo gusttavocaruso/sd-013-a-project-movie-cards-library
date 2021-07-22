51073c8cfc012f71fb621984180eae1ddaba89e9
Debora Teodorico <debora.steodorico@gmail.com>
2021-07-22 17:48:09 -0300

    Adicionando questao 4,5,6,7,8

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -3,9 +3,12 @@ import React from 'react';
 
 class MovieCard extends React.Component {
   render() {
+    const { movie } = this.props;
+    const { title, subtitle, storyline, imagePath, rating } = movie;
     return (
       <section>
-        <p>teste!</p>
+        <img src={ imagePath } alt={ title } />
+        <h4>{ title }</h4>
       </section>
     );
   }
