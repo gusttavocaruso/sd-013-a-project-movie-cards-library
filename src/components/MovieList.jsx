042d8c331321494608597be5280a89d5fa3358a2
Julia Baptista <baptista.julia@yahoo.com.br>
2021-07-22 16:30:11 -0300

    Quesito 3

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,14 +1,18 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 class MovieList extends React.Component {
   render() {
     const { movies } = this.props;
+    console.log(movies);
     return (
-      <header>
-        <h1>test</h1>
-      </header>
+      <p>teste</p>
     );
   }
 }
+
+MovieList.propTypes = {
+  movies: PropTypes.arrayOf(PropTypes.object).isRequired,
+};
 
 export default MovieList;
