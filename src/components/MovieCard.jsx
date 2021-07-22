8d0dd6de97b190d7daa0006e9ec9df9f682e72fc
Ingo Reichert Junior <ingoreichertjr@gmail.com>
2021-07-22 17:31:24 -0300

    Adiciona requisitos 3,4 e 5

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,1 +1,11 @@
-// implement MovieCard component
+import React from 'react';
+
+class MovieCard extends React.Component {
+  render() {
+    return (
+      <h1>Renderiza algo</h1>
+    );
+  }
+}
+
+export default MovieCard;
---
+++ src/components/MovieList.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import MovieCard from './MovieCard';
+
+class MovieList extends React.Component {
+  render() {
+    const { movies } = this.props;
+    return (
+      <div>
+        { movies.map((movie) => <MovieCard key={ movie.title } movies={ movie } />) }
+      </div>
+    );
+  }
+}
+
+MovieList.propTypes = {
+  movies: PropTypes.arrayOf(PropTypes.shape({
+    title: PropTypes.string,
+    subtitle: PropTypes.string,
+    storyline: PropTypes.string,
+    rating: PropTypes.number,
+    imagePath: PropTypes.string,
+  })),
+};
+
+MovieList.defaultProps = {
+  movies: [],
+};
+
+export default MovieList;
