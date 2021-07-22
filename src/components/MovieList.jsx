f5fe8c3e4161f7d9c5e8b1d9087f68c791f9c963
Pedro Muniz <pedro.muniz@aluno.uece.br>
2021-07-22 16:44:44 -0300

    req 4 a 10

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,9 +1,23 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import MovieCard from './MovieCard';
 
-function MovieList() {
-  return (
-    <div>!!</div>
-  );
+class MovieList extends React.Component {
+  render () {
+    const { movies } = this.props;
+    const m = movies.map((movie) => <MovieCard key={ movie.title } movie={movie} />);
+
+    return (
+      <main className="movie-list">
+        { m }
+      </main>
+    );
+  }
+  
 }
+
+MovieList.propTypes = {
+  movies: PropTypes.arrayOf(PropTypes.object).isRequired,
+};
 
 export default MovieList;
