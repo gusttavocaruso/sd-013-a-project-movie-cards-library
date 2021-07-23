dd81c721fe3437a33ba75117798549a5502295a1
FelipeNevess <felipe012neves@gmail.com>
2021-07-22 21:03:21 -0300

    Projeto finalizado por completo

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,5 +1,6 @@
 // implement MovieList component here
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import MovieCard from './MovieCard';
 
 class MovieList extends Component {
@@ -13,5 +14,9 @@ class MovieList extends Component {
     );
   }
 }
+
+MovieList.propTypes = {
+  movies: PropTypes.arrayOf(PropTypes.object).isRequired,
+};
 
 export default MovieList;
