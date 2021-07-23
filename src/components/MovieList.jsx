ca78218f2c5d882e0e756f5d656c97f6a41fbb4b
Japa <wesleyy_@live.com>
2021-07-23 20:53:33 -0300

    Adicionado requisitos 1 ao 10 e 15 e 16

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,1 +1,23 @@
-// implement MovieList component here
+import React, { Component } from 'react';
+import PropsTypes from 'prop-types';
+import MovieCard from './MovieCard';
+
+class MovieList extends Component {
+  render() {
+    const { movies } = this.props;
+
+    return (
+      <section>
+        <div>
+          {movies.map((movie) => <MovieCard key={ movie.title } movie={ movie } />)}
+        </div>
+      </section>
+    );
+  }
+}
+
+MovieList.propTypes = {
+  movies: PropsTypes.arrayOf(PropsTypes.object).isRequired,
+};
+
+export default MovieList;
