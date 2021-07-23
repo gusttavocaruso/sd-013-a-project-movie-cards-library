42e8a53788e1b0cc8a31eeb9889e2c43b22ddf2b
Emanoel Mattos <emanoelmattos13@gmail.com>
2021-07-22 22:31:26 -0300

    adiciona o requisito 6

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,7 +1,15 @@
 import React from 'react';
+import MovieCard from './MovieCard';
 
 class MovieList extends React.Component {
-  render()
+  render() {
+    const { movies } = this.props;
+    return(
+      <div>
+      {movies.map((movie) => <MovieCard movie={movie} key={movie.title} />)}
+      </div>
+    )
+  }
 }
 
 export default MovieList;
