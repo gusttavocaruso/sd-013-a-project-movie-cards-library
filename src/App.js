5008a020bb53c87cda31d01a57958c30b673a45e
Adriano Monteiro <adrianomonteiroweb@gmail.com>
2021-07-23 12:40:24 -0300

    requisito 3

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
 import React from 'react';
 import './App.css';
 import Header from './components/Header';
+import MovieList from './components/MovieList';
+import movies from './data';
 
 function App() {
   return (
     <div className="App">
       <Header />
+      {movies.map(({ title, subtitle, storyLine, rating, imagePath }, i) => (
+        <MovieList
+          key={ i }
+          title={ title }
+          subtitle={ subtitle }
+          storyLine={ storyLine }
+          rating={ rating }
+          imagePath={ imagePath }
+        />
+      ))}
     </div>
   );
 }
--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,1 +1,18 @@
-// implement MovieList component here
+import React from 'react';
+class MovieList extends React.Component {
+  render() {
+    const { title, subtitle, storyLine, rating, imagePath } = this.props;
+
+    return (
+      <section>
+          <h1>Título: { title }</h1>
+          <h2>Subtítulo: { subtitle }</h2>
+          <p>Sinópse: { storyLine }</p>
+          <p>Nota: { rating }</p>
+          <img src={ imagePath } alt="Série" />
+      </section>
+    );
+  }
+}
+
+export default MovieList;
