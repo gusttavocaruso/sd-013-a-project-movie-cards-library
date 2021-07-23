039ee085ff82e7dd7c117f97695a3f0d208571b8
Yuri Garrido <y.garrido.yuri@gmail.com>
2021-07-22 23:35:39 -0300

    envia requisito 03

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,1 +1,18 @@
 // implement MovieCard component here
+import React from 'react';
+
+class MovieCard extends React.Component {
+  render() {
+    const { objct } = this.props;
+    return (
+      <div>
+        <img src={ objct.imagePath } alt={ objct.title } />
+        <h4>{ objct.title }</h4>
+        <h5>{ objct.subtitle }</h5>
+        <p>{ objct.storyline }</p>
+      </div>
+    );
+  }
+}
+
+export default MovieCard;
--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,1 +1,13 @@
-// implement MovieList component here
+import React from 'react';
+import MovieCard from './MovieCard';
+import movies from '../data';
+
+class MovieList extends React.Component {
+  render() {
+    return (
+      movies.map((mov, index) => <MovieCard objct={ mov } key={ index } />)
+    );
+  }
+}
+
+export default MovieList;
