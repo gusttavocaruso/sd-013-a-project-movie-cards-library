a074f9ec321454ac00bbff04ed1a177051f2bb4a
Isabella Antunes <bella.antunes2@gmail.com>
2021-07-23 11:47:04 -0300

    Ajustanto o CSS

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,11 @@
+/* feito com a ajuda das mulheres da Turma 13 A - Jaqueline Silva, Vanessa Rios, Debora Teodorico, Juliane Marques, Heleny Flavia, Julia Baptista, Aline Eiko Hoshino, Elaine Moreira, Luiza Antiques, Sthefany Caroline, Jessica Queiroz, Lanai Conceicao e tambem Pedro Delicoli. */
 import React from 'react';
 
 class Header extends React.Component {
   render() {
     return (
-      <header>
-        <h1>Movie Cards Library</h1>
+      <header className="movie-card-header">
+        <h1 className="page-title">Movie Cards Library</h1>
       </header>
     );
   }
