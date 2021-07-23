c689795d95439e1302a13fe7154ed94067b03235
Emídio Rebouças <emidioreb@gmail.com>
2021-07-23 08:17:22 -0300

    requirement 8 and 9 ok

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -4,11 +4,12 @@ import PropTypes from 'prop-types';
 // Aonde eu estou organizando os meus dados que la do data: sequencia, oq eu quero de qual forma eu apresento.
 class MovieCard extends React.Component {
   render() {
+    // desentruturando os dados que esta vindo do elemento pai(MovieList.jsx) e filtrando do jeito que eu quero.
     const { movie: { title, subtitle, storyline, imagePath, rating } } = this.props;
     return (
       <div>
-        <p>{title}</p>
-        <p>{subtitle}</p>
+        <h4>{title}</h4>
+        <h5>{subtitle}</h5>
         <p>{storyline}</p>
         <img src={ imagePath } alt={ title } />
         <p>{rating}</p>
--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -5,6 +5,7 @@ import MovieCard from './MovieCard';
 // map vai fazer eu passar por todos os filmes da minha lista pegando os valores q eu quero
 class MovieList extends React.Component {
   render() {
+    // pegando os dados que esta vindo do elemento pai(App.js), estou desestruturando e colocando no map.
     const { movies } = this.props;
     return (
       <div>
