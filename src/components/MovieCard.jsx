d357c49c3b86a7f574c773eff97f6c29695767fc
Fabiola Moutinho <fabiolamoutinho@hotmail.com>
2021-07-22 23:05:06 -0400

    Requisito 7

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -2,9 +2,11 @@ import React from 'react';
 
 class MovieCard extends React.Component {
   render() {
+    const { movie } = this.props;
+    const { title, subtitle, storyline, imagePath, rating } = movie;
     return (
       <div>
-
+        <img className="movie-card-image" src={ imagePath } alt="movieImage"/>
       </div>
     );
   }
