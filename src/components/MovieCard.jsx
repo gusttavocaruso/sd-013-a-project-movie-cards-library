c25dbff37ebf755c5b05eba66f60fbbdf970f1a6
junglejf <jfcchagas@id.uff.br>
2021-07-22 23:11:30 -0300

    Criados Movie Card e MovieList , requisitos 3 , 4 ,5 ,6

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,1 +1,33 @@
-// implement MovieCard component here
+import React from 'react';
+import PropTypes from 'prop-types';
+class MovieCard extends React.Component {
+  render() {
+    const { movieInfo } = this.props;
+    console.log(movieInfo);
+    return (
+      <section className='movie-card'>
+        <h1>
+          { movieInfo.title }
+        </h1>
+        <p>{ movieInfo.subtitle }</p>
+        <p>{ movieInfo.storyline }</p>
+        <p>{ movieInfo.rating }</p>
+        <img src={ movieInfo.imagePath } alt={ movieInfo.title } />
+      </section>
+    );
+  }
+}
+
+MovieCard.PropTypes = { 
+  movieInfo: PropTypes.shape({
+    title: PropTypes.string,
+    subtitle: PropTypes.string,
+    storyline: PropTypes.string,
+    rating: PropTypes.number,
+    imagePath: PropTypes.string,
+  })
+}
+
+MovieCard.defaultProps = { movieInfo: {} }
+
+export default MovieCard;
--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,1 +1,30 @@
-// implement MovieList component here
+import React from 'react';
+import MovieCard from './MovieCard';
+import PropTypes from 'prop-types';
+
+class MovieList extends React.Component {
+
+  render() {
+    
+    const { movieData } = this.props;
+    console.log("linha 11",movieData);
+    return (
+      <div className='movie-list'>
+       { movieData.map((movie, index) => <MovieCard key=  {index} movieInfo= { movie }/>) }
+       </div>
+    );
+  }
+}
+
+MovieList.PropTypes = {
+  movieData: PropTypes.arrayOf(PropTypes.shape({
+    title: PropTypes.string,
+    subtitle: PropTypes.string,
+    storyline: PropTypes.string,
+    rating: PropTypes.number,
+    imagePath: PropTypes.string,
+  })),
+}
+
+MovieList.defaultProps = { movieData: [] }
+export default MovieList;
