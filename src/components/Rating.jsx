c154895766f775dec3b81d771ad4179858781142
Jackson Santana <onjacksm@gmail.com>
2021-07-23 17:26:30 -0300

    Adiciona requisitos 11 a 14

---
+++ src/components/Rating.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+class Rating extends React.Component {
+  render() {
+    const { rating } = this.props;
+    return (
+      <p className="rating">{ rating }</p>
+    );
+  }
+}
+
+export default Rating;
