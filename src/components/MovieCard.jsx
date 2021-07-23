a146ab7962e4b3b74874e907c02d3432bf53cd4c
Emídio Rebouças <emidioreb@gmail.com>
2021-07-23 07:53:42 -0300

    requirement 4,5,6 and 7 ok

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,1 +1,29 @@
-// implement MovieCard component here
+import React from 'react';
+import PropTypes from 'prop-types';
+
+// Aonde eu estou organizando os meus dados que la do data: sequencia, oq eu quero de qual forma eu apresento.
+class MovieCard extends React.Component {
+  render() {
+    const { movie: { title, subtitle, storyline, imagePath, rating } } = this.props;
+    return (
+      <div>
+        <p>{title}</p>
+        <p>{subtitle}</p>
+        <p>{storyline}</p>
+        <img src={ imagePath } alt={ title } />
+        <p>{rating}</p>
+      </div>
+    );
+  }
+}
+MovieCard.propTypes = {
+  movie: PropTypes.shape({
+    title: PropTypes.string,
+    subtitle: PropTypes.string,
+    storyline: PropTypes.string,
+    rating: PropTypes.number,
+    imagePath: PropTypes.string,
+  }).isRequired,
+};
+
+export default MovieCard;
--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,11 +1,20 @@
 import React from 'react';
+import PropsTypes from 'prop-types';
+import MovieCard from './MovieCard';
 
+// map vai fazer eu passar por todos os filmes da minha lista pegando os valores q eu quero
 class MovieList extends React.Component {
   render() {
+    const { movies } = this.props;
     return (
-      <movie />
+      <div>
+        { movies.map((movie) => <MovieCard key={ movie.title } movie={ movie } />)}
+      </div>
     );
   }
 }
 
+MovieList.propTypes = {
+  movies: PropsTypes.arrayOf(PropsTypes.object).isRequired,
+};
 export default MovieList;
