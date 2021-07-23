4c059ce0d96016933cf16f51b04321c695855fb5
Gustavo-Jezini <gustavojezini@gmail.com>
2021-07-23 09:42:26 -0300

    Rating

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import Rating from './Rating';
 
 class MovieCard extends Component {
   render() {
     const { movie } = this.props;
 
-    const { title, subtitle, storyline, imagePath } = movie;
+    const { title, subtitle, storyline, imagePath, rating } = movie;
 
     return (
       <section>
@@ -13,6 +14,9 @@ class MovieCard extends Component {
         <h4>{ title }</h4>
         <h5>{ subtitle }</h5>
         <p>{ storyline }</p>
+        <div>
+          <Rating rating={ rating } />
+        </div>
       </section>
     );
   }
