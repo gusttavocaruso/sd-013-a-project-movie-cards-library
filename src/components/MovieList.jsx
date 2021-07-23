0d5359ec321f74d45d353b1cdfefb5e1c67218d9
Fabiola Moutinho <fabiolamoutinho@hotmail.com>
2021-07-22 22:37:58 -0400

    Requisito 4, 5 e 6

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,9 +1,13 @@
 import React from 'react';
+import MovieCard from './MovieCard';
 
 class MovieList extends React.Component {
   render() {
+    const { movies } = this.props;
     return (
-      <h1>Vamo que vamo</h1>
+      <div className="movie-list">
+        {movies.map((movInfo) => <MovieCard key={ movInfo.title } movies={ movInfo } />)}
+      </div>
     );
   }
 }
