e55f62830aa4640ec4912adade281d251cfd0bb7
ivandosss <ivanndosssilva9389@gmail.com>
2021-07-23 17:13:55 -0300

    Requisitos 3,4,5,6,7,8,9,10,11,12,13,14,

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,10 +1,23 @@
 import React from 'react';
+import Rating from './Rating';
 
-class MovieList extends React.Component {
+class MovieCards extends React.Component {
   render() {
-    const {} = this.props;
+    const { movie } = this.props;
+    const { title, subtitle, storyline, rating, imagePath } = movie;
     return (
 
-    )
+      <section className="movie-card">
+        <div className="movie-card-body">
+          <img className="movie-card-image" src={ imagePath } alt="imagem" />
+          <h4 className="movie-card-title">{ title }</h4>
+          <h5 className="movie-card-subtitle">{ subtitle }</h5>
+          <p className="movie-card-storyline">{ storyline }</p>
+          <Rating rating={ rating } />
+        </div>
+      </section>
+    );
   }
 }
+
+export default MovieCards;
--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,1 +1,16 @@
-// implement MovieList component here
+import React from 'react';
+import MovieCards from './MovieCard';
+
+class MovieList extends React.Component {
+  render() {
+    const { movies } = this.props;
+
+    return (
+      <section className="movie-list">
+        {movies.map((movie) => <MovieCards key={ movie.title } movie={ movie } />)}
+      </section>
+    );
+  }
+}
+
+export default MovieList;
