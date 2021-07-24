d8fdfd45a52fd4bc2291836f31a8b187f79dda90
Douglas dos Santos <doug.frutuoso@gmail.com>
2021-07-24 11:18:11 -0300

    desafio 9 feito

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -12,6 +12,11 @@ class MovieCard extends React.Component {
           { title }
           {' '}
         </h4>
+        <h5>
+          {' '}
+          { subtitle}
+        </h5>
+
       </div>
     );
   }
