654eb521b4fea32e75910e1f364ef891a2586914
icaroMelo <exemployt@gmail.com>
2021-07-24 03:19:32 -0300

    Requisito 7

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,16 +1,22 @@
 // implement MovieCard component here
-// import PropTypes from 'prop-types';
-
 import React from 'react';
+import Rating from './Rating';
+import PropTypes from 'prop-types';
 
 class MovieCard extends React.Component {
   render() {
+    const { movie } = this.props;
+    const { title, imagePath, subtitle, storyline, rating, link } = movie;
     return (
       <div>
-        Movie Card
+        <img src={ imagePath } alt={ title } />
       </div>
     );
   }
 }
+
+// MovieCard.propTypes = {
+
+// }
 
 export default MovieCard;
--- src/components/Rating.jsx
+++ src/components/Rating.jsx
@@ -1,15 +1,21 @@
 // implement Rating component here
 
 import React from 'react';
+import PropType from 'prop-types';
 
 class Rating extends React.Component {
   render() {
+    const { rate } = this.props;
     return (
       <div>
-        Rate
+        <p>{ rate }</p>
       </div>
     );
   }
 }
+
+Rating.propType = {
+  rate: PropType.number.isRequired,
+};
 
 export default Rating;
