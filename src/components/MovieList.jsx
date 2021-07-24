a824a88c4891f138cc5901ad7ca64c5d92cf102e
icaroMelo <exemployt@gmail.com>
2021-07-24 02:04:13 -0300

    Testando Requisito

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,25 +1,23 @@
 // implement MovieList component here
 // https://pt-br.reactjs.org/docs/components-and-props.html
-
-import PropTypes from 'prop-types';
-import MovieCard from './MovieCard';
 import React from 'react';
+// import PropTypes from 'prop-types';
+import MovieCard from './MovieCard';
 import movies from '../data';
 
 class MovieList extends React.Component {
   render() {
-    const { movies } = this.props;
-
+    // const { movies } = this.props;
     return (
-      <main>
-       { movies.map((movie) => <MovieCard key={ movie.title } movie= { movie }/>)}
-      </main>
+      <div>
+        {movies.map((movie) => <MovieCard key={ movie.title } movie={ movie } />)}
+      </div>
     );
   }
 }
 
-MovieList.propTypes = {
-  movies: PropTypes.arrayOf(PropTypes.object).isRequired,
-};
+// MovieList.propTypes = {
+//   movies: PropTypes.arrayOf(PropTypes.object).isRequired,
+// };
 
 export default MovieList;
