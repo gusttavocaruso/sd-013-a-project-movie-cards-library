47f4c9ba8a12ec91463f5899471465a62dc53e1c
Douglas dos Santos <doug.frutuoso@gmail.com>
2021-07-24 11:22:11 -0300

    desafio 10 feito

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -6,17 +6,28 @@ class MovieCard extends React.Component {
 
     return (
       <div>
-        <img src={ imagePath } alt={ title } />
-        <h4>
-          {' '}
-          { title }
-          {' '}
-        </h4>
-        <h5>
-          {' '}
-          { subtitle}
-        </h5>
-
+        <div>
+          <img src={ imagePath } alt={ title } />
+          <h4>
+            {' '}
+            { title }
+            {' '}
+          </h4>
+          <h5>
+            {' '}
+            { subtitle}
+          </h5>
+          <p>
+            {' '}
+            {storyline}
+          </p>
+        </div>
+        <div>
+          <p>
+            {' '}
+            {rating}
+          </p>
+        </div>
       </div>
     );
   }
