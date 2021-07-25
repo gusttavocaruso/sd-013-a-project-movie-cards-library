88f00c4590e0939a46b5c26641d7cfc451ae6379
Cassyel <cassyel1967@gmail.com>
2021-07-24 21:44:13 -0300

    Realiza renderizaçao do componente MovieCard no componente MovieList

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,12 +1,17 @@
 import React from 'react';
+import PropType from 'prop-types';
 import MovieCard from './MovieCard';
 
 class MovieList extends React.Component {
   render() {
+    const { movies } = this.props;
     return (
-      <MovieCard />
+      movies.map((movie, index) => <MovieCard key={ index } movie={ movie } />)
     );
   }
 }
 
+MovieList.propTypes = {
+  movies: PropType.arrayOf.isRequired,
+};
 export default MovieList;
