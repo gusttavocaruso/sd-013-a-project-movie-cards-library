0501e594cdff064eada689591caa6f4eab8c856b
Sethano <vitorcard1720@gmail.com>
2021-07-25 12:35:57 -0300

    projeto finalizado

---
+++ src/components/Header.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class Header extends React.Component {
+  render() {
+    return (
+      <header>
+        <h1 className="movie-card-header page-title">Movie Cards Library</h1>
+      </header>
+    );
+  }
+}
+
+export default Header;
