cc430b8087e43a43079838877d5a4a94d4928bf1
Douglas dos Santos <doug.frutuoso@gmail.com>
2021-07-25 11:40:50 -0300

    desafio 11 feito

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import Rating from './Rating';
 
 class MovieCard extends React.Component {
   render() {
@@ -9,25 +10,16 @@ class MovieCard extends React.Component {
         <div>
           <img src={ imagePath } alt={ title } />
           <h4>
-            {' '}
             { title }
-            {' '}
           </h4>
           <h5>
-            {' '}
             { subtitle}
           </h5>
           <p>
-            {' '}
             {storyline}
           </p>
         </div>
-        <div className="rating">
-          <p>
-            {' '}
-            {rating}
-          </p>
-        </div>
+        <Rating />
       </div>
     );
   }
