cae804ba04416edb51b5401160659d5b5fe07dfc
Seu nomecd <canaldogildosantos@gmail.com>
2021-07-25 02:31:38 -0300

    corrige erro de lint

--- src/components/Rating.jsx
+++ src/components/Rating.jsx
@@ -13,13 +13,10 @@ class Rating extends React.Component {
 }
 
 Rating.propTypes = {
-  rating: PropTypes.shape({
-    rating: PropTypes.number.isRequired,
-  }),
-};
+  rating: PropTypes.shape({ rating: PropTypes.number }) };
 
-// Rating.defaultProps = {
-//   rating: 0,
-// };
+Rating.defaultProps = {
+  rating: 0,
+};
 
 export default Rating;
