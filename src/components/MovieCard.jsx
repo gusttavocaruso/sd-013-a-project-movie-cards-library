a7ee48654b6e44db0a9e28519e330fb3b917c2e0
Cassyel <cassyel1967@gmail.com>
2021-07-24 22:55:58 -0300

    Realiza correçao estilizaçao no Rating

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -14,7 +14,9 @@ class MovieCard extends React.Component {
           <h5 className="movie-card-subtitle">{ subtitle }</h5>
           <p className="movie-card-storyline">{ storyline }</p>
         </div>
-        <Rating rating={ rating } />
+        <div className="movie-card-rating">
+          <Rating rating={ rating } />
+        </div>
       </div>
     );
   }
