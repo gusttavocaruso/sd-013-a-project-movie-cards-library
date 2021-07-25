b60e059e307a3c6649e7934f6f5162a360b2ddfc
rene-t13-trybe <cleytonacompanha@gmail.com>
2021-07-25 20:51:06 -0300

    Adiciona Requisitos de 4-7 e 15,16

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,13 +1,20 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import MovieCard from './MovieCard';
 
 class MovieList extends React.Component {
   render() {
+    const { movies } = this.props;
     return (
-      <header>
-        <h1>Oi</h1>
-      </header>
+      <div>
+        {movies.map((movie) => <MovieCard key={ movie.title } movie={ movie } />)}
+      </div>
     );
   }
 }
+
+MovieList.propTypes = {
+  movies: PropTypes.arrayOf(PropTypes.object).isRequired,
+};
 
 export default MovieList;
