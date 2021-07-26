f4cd570a497872cc11c1abc55e3e769918f9b0eb
ArthurMGoulart <budiarthur@gmail.com>
2021-07-26 10:39:01 -0300

    Realizando todos requisitos e o lint

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import './App.css';
-import Header from './components/Header.jsx'
+import movies from './data';
+import Header from './components/Header';
+import MovieList from './components/MovieList';
+
 function App() {
   return (
     <div className="App">
-      <Header name="Movie Cards Library"/>
+      <Header />
+      <MovieList movies={ movies } />
     </div>
   );
 }
 
 export default App;
-// Teste
--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,1 +1,43 @@
 // implement MovieCard component here
+import React from 'react';
+import PropTypes from 'prop-types';
+import Rating from './Rating';
+
+class MovieCard extends React.Component {
+  render() {
+    const { movie } = this.props;
+    const { imagePath, title, subtitle, storyline, rating } = movie;
+    return (
+      <div className="movie-card">
+        <h4 className="movie-card-title">{ title }</h4>
+        <h5 className="movie-card-subtitle">{ subtitle }</h5>
+        <p className="movie-card-storyline">{ storyline }</p>
+        <img src={ imagePath } alt="Poster do Filme" />
+        <Rating rating={ rating } />
+      </div>
+    );
+  }
+}
+
+MovieCard.propTypes = {
+  movie: PropTypes.shape({
+    imagePath: PropTypes.string.isRequired,
+    title: PropTypes.string.isRequired,
+    subtitle: PropTypes.string.isRequired,
+    storyline: PropTypes.string.isRequired,
+    rating: PropTypes.number.isRequired,
+  }),
+
+};
+
+MovieCard.defaultProps = {
+  movie: {
+    imagePath: '',
+    title: 'default-title',
+    subtitle: 'default-subtitle',
+    storyline: 'default-storyline',
+    rating: 0,
+  },
+};
+
+export default MovieCard;
