5bd23c8c8064de1670e553aaf84e9095e23cec4c
Marcelo Alessandro <marceloouropreto@gmail.com>
2021-07-26 19:30:36 -0300

    Corrige lint do Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
+
 class Header extends React.Component {
-    render() {
-        return (
-            <header className="Movie Cards Library">
-                <h1 className="page-title">Movie Cards Library</h1>
-            </header>
-        );
-    }
+  render() {
+    return (
+      <header className="Movie Cards Library">
+        <h1 className="page-title">Movie Cards Library</h1>
+      </header>
+    );
+  }
 }
 export default Header;
