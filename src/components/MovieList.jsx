c04404a3254c0c2c9c91c0830b6ebbff93dc2cc0
Eduardo Rossi <eduardo.g.rossi@hotmail.com>
2021-07-26 18:32:05 -0300

    requisito 6

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,13 +1,24 @@
 import React from 'react';
+import PropType from 'prop-types';
 import MovieCard from './MovieCard';
 
 class MovieList extends React.Component {
   render() {
     const { movies } = this.props;
     return (
-      movies.map((movie) => <MovieCard movie={ movie } />)
+      movies.map((movie) => <MovieCard key={ movie } movie={ movie } />)
     );
   }
 }
+
+MovieList.propTypes = {
+  movies: PropType.arrayOf(PropType.shape({
+    title: PropType.string,
+    subtitle: PropType.string,
+    storyline: PropType.string,
+    rating: PropType.number,
+    imagePath: PropType.string,
+  })).isRequired,
+};
 
 export default MovieList;
