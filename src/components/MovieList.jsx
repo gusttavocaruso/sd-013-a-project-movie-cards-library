bb6b9898d7bcca01bbe4c9f0235ade0c0c402463
LuanRodriguesep <luanrodriguesep@yahoo.com.br>
2021-07-26 02:44:24 -0300

    criando componentes MovieList e MovieCard parte 01

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,1 +1,35 @@
-// implement MovieList component here
+import React from 'react';
+import PropTypes from 'prop-types';
+import MovieCard from './MovieCard';
+
+class MovieList extends React.Component {
+  render() {
+    const { movies } = this.props;
+
+    return (
+      <div className="list-movie">
+        {movies.map((movie) => <MovieCard key={ movie.title } movie={ movie } />)}
+      </div>
+    );
+  }
+}
+
+MovieList.propTypes = {
+  movies: PropTypes.arrayOf(PropTypes.object).isRequired,
+};
+
+// MovieList.propsTypes = {
+//   movies: PropTypes.arrayOf(PropTypes.shape({
+//     title: PropTypes.string,
+//     subtitle: PropTypes.string,
+//     storyline: PropTypes.string,
+//     rating: PropTypes.number,
+//     imagePath: PropTypes.string,
+//   })),
+// };
+
+// MoviesList.defaultProps = {
+//   movie: [],
+// };
+
+export default MovieList;
