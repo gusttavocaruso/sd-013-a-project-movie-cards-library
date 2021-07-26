0f55ca2027f62b3f1f14b47f571491f3ec7fa54d
Eduardo Rossi <eduardo.g.rossi@hotmail.com>
2021-07-26 18:45:58 -0300

    requisito 9

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -8,6 +8,7 @@ class MovieCard extends React.Component {
       <section>
         <img src={ movie.imagePath } alt="" />
         <h4>{movie.title}</h4>
+        <h5>{movie.subtitle}</h5>
       </section>
     );
   }
