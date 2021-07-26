92b260040769e33c682c029c3805f7d2b5979914
Vanessa <vanessamrios@gmail.com>
2021-07-26 18:40:34 -0300

    Adiciona referencias

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,6 @@
 // implement Header component here
+// feito com a ajuda das mulheres da Turma 13 A - Jaqueline Silva, Vanessa Rios, Debora Teodorico, Isabella Antunes, Heleny Flavia, Julia Baptista, Aline Eiko Hoshino, Elaine Moreira, Luiza Antiques, Sthefany Caroline, Jessica Queiroz, Lanai Conceicao e tambem Pedro Delicoli
+
 import React from 'react';
 
 class Header extends React.Component {
--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,4 +1,6 @@
 // implement MovieCard component here
+// feito com a ajuda das mulheres da Turma 13 A - Jaqueline Silva, Vanessa Rios, Debora Teodorico, Isabella Antunes, Heleny Flavia, Julia Baptista, Aline Eiko Hoshino, Elaine Moreira, Luiza Antiques, Sthefany Caroline, Jessica Queiroz, Lanai Conceicao e tambem Pedro Delicoli
+
 import React from 'react';
 import PropTypes from 'prop-types';
 import Rating from './Rating';
