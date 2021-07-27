01e04cc3af82bec5be28e3484711e2b2aa2e8511
Wesley Maia <maia.weol@gmail.com>
2021-07-27 11:45:06 -0300

    Refatorando o código

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -12,7 +12,7 @@ class MovieCard extends React.Component {
         <h4 className="movie-card-title">{ title }</h4>
         <h5 className="movie-card-subtitle">{ subtitle }</h5>
         <p className="movie-card-storyline">{ storyline }</p>
-        <img src={ imagePath } alt={ title } className="movie-card-image"/>
+        <img src={ imagePath } alt={ title } className="movie-card-image" />
         <Rating rating={ rating } />
       </section>
     );
