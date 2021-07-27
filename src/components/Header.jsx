013475bc34c9431c2b00b98a42f3d462b2d344db
Jaqueline Silva <jaqueline.natario@gmail.com>
2021-07-27 18:56:04 -0300

    Adiciona comentários no componete 'Header'

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,11 +3,14 @@ import React from 'react';
 class Header extends React.Component {
   render() {
     return (
-      <header classeName="movie-card-header">
-        <h1 classeName="page-title">Movie Cards Library</h1>
+      <header className="movie-card-header">
+        <h1 className="page-title">Movie Cards Library</h1>
       </header>
     );
   }
 }
 
 export default Header;
+
+//
+// Nesse arquivo foi criado um componente chamado 'Header' com a className="movie-card-header", tendo como filho uma tag 'h1', com a classeName="page-title"
