6cd82c4ab20a671e5fa151c17b6e6867e8e9eb4a
Micael Santos <micaelmaicon@hotmail.com>
2021-07-27 10:54:34 -0300

    Requirement 1

---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+class Header extends Component {
+  render() {
+    return (
+      <header className="movies-card-hearder">
+        <h1 className="title-content">Movie Cards Library</h1>
+      </header>);
+  }
+}
+
+export default Header;
