48205608af94914308d9008bcb4d9014dee5f6f9
Gabriel Priss <gabrielpriss@hotmail.com>
2021-07-27 17:05:54 -0300

    resolve lint

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import Rating from './Rating'
+import Rating from './Rating';
 
 class MovieCard extends React.Component {
   render() {
@@ -11,7 +11,7 @@ class MovieCard extends React.Component {
         <h1 className="movie-card-title">{movie.title}</h1>
         <h2 className="movie-card-subtitle">{movie.subtitle}</h2>
         <p className="movie-card-storyline">{movie.storyline}</p>
-        <Rating rating={ movie.rating }/>
+        <Rating rating={ movie.rating } />
       </div>
     );
   }
