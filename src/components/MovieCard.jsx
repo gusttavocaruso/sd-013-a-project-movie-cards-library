7e389cffb8f28fbaaafad8066a79f613cefbd27b
Gabriel Priss <gabrielpriss@hotmail.com>
2021-07-27 17:55:34 -0300

    requisitos finais e lint

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -8,8 +8,8 @@ class MovieCard extends React.Component {
     return (
       <div className="movie-card mobie-card-body">
         <img className="movie-card-image" src={ movie.imagePath } alt="movie_image" />
-        <h1 className="movie-card-title">{movie.title}</h1>
-        <h2 className="movie-card-subtitle">{movie.subtitle}</h2>
+        <h4 className="movie-card-title">{movie.title}</h4>
+        <h5 className="movie-card-subtitle">{movie.subtitle}</h5>
         <p className="movie-card-storyline">{movie.storyline}</p>
         <Rating rating={ movie.rating } />
       </div>
--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -7,7 +7,7 @@ class MovieList extends React.Component {
     const { movies } = this.props;
     return (
       <div className="movie-list">
-        {movies.map((info) => <MovieCard key={ info.title } movie={ info } />)}
+        {movies.map((object) => <MovieCard key={ object.title } movie={ object } />)}
       </div>
     );
   }
