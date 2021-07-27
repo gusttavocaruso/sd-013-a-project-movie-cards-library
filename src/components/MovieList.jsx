8777546304a031cd1fa7947ca1e0f2f57b211021
Felipe Dias <felipeferreiradias1@gmail.com>
2021-07-27 00:17:29 -0300

    adiciona requisitos restantes e corrige lint

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,5 +1,6 @@
 // implement MovieList component here
 import React from 'react';
+import PropTypes from 'prop-types';
 import MovieCard from './MovieCard';
 
 class MovieList extends React.Component {
@@ -10,5 +11,9 @@ class MovieList extends React.Component {
     );
   }
 }
+
+MovieList.propTypes = {
+  movies: PropTypes.arrayOf(PropTypes.object).isRequired,
+};
 
 export default MovieList;
