830028d45ba9bf2daf021bfe39f5cda03aebdca3
Marcelo Alessandro <marceloouropreto@gmail.com>
2021-07-26 22:48:13 -0300

    Renderizar tag img como atrbuto src e propriedade imagePath de objeto prop; Recebe prop como objeto com as propriedades title, subtitle, storyline, imagePath e rating

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,1 +1,17 @@
 // implement MovieCard component here
+import React from 'react';
+import PropType from 'prop-types';
+import Rating from './Rating';
+import '../App.css';
+
+class MovieCard extends React.Component {
+  render() {
+    const { movie } = this.props;
+    const { imagePath, title, subtitle, storyline, rating } = movie;
+    return (
+      <section className="movie-card-body movie-card">
+        <img className="movie-card-image" src={ imagePath } alt={ title } />
+        <Rating rating={ rating } />
+      </section>
+    )
+  }
