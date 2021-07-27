494e67e7ef77e4ccb8a146e84d44e1941a0a2591
Felipe Dias <felipeferreiradias1@gmail.com>
2021-07-26 22:40:13 -0300

    adiciona requisito 8,9,10

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -6,11 +6,11 @@ class MovieCard extends React.Component {
   render () {
     return (
       <div>
-        <img src={this.props.imagePath} alt={this.props.title} />
-        <h4>{this.props.title}</h4>
-        <h5>{this.props.subtitle}</h5>
-        <p>{this.props.storyline}</p>
-        <Rating rating = { this.props.rating }/>
+        <img src='{this.props.imagePath}' alt='{this.props.title}' />
+        <h4>{this.props.movie.title}</h4>
+        <h5>{this.props.movie.subtitle}</h5>
+        <p>{this.props.movie.storyline}</p>
+        <Rating rating = { this.props.movie.rating }/>
       </div>
     )
   }
