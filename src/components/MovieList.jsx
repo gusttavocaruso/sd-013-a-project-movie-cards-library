d4ca6a33bf44c3f6f63671ffc789876468c559f6
Walace Nascimento <wlacens@hotmail.com>
2021-07-26 21:03:28 -0300

    Resolve o requisito 3 e 16

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,1 +1,15 @@
 // implement MovieList component here
+import React from 'react';
+import movies from '../data';
+
+class MovieList extends React.Component {
+  render() {
+    return (
+      <div>
+        <movie moviesInfo={ movies } />
+      </div>
+    );
+  }
+}
+
+export default MovieList;
