56f287abe070f3de9818cd1bbcae323e2d864ce6
Marcelo Alessandro <marceloouropreto@gmail.com>
2021-07-26 23:00:08 -0300

    Título do filme na tag h4 na propriedade title; Subtítulo na tag h5 na propriedade subtitle; Renderiza sinopse na tag p na propriedade storyline

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -11,6 +11,9 @@ class MovieCard extends React.Component {
     return (
       <section className="movie-card-body movie-card">
         <img className="movie-card-image" src={ imagePath } alt={ title } />
+        <h4 className="movie-card-title">{ title }</h4>
+        <h5 className="movie-card-subtitle">{ subtitle }</h5>
+        <p className="movie-card-storyline">{ storyline }</p>
         <Rating rating={ rating } />
       </section>
     )
