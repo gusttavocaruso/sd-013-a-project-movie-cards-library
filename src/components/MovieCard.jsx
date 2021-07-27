d1218b7417ba64d01c575ee0e544ef1e7bd79d70
Wesley Maia <maia.weol@gmail.com>
2021-07-27 11:51:10 -0300

    Estilização com CSS

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -8,7 +8,7 @@ class MovieCard extends React.Component {
     const { movie } = this.props;
     const { title, subtitle, imagePath, storyline, rating } = movie;
     return (
-      <section className="movie-card, movie-card-body">
+      <section className="movie-card">
         <h4 className="movie-card-title">{ title }</h4>
         <h5 className="movie-card-subtitle">{ subtitle }</h5>
         <p className="movie-card-storyline">{ storyline }</p>
