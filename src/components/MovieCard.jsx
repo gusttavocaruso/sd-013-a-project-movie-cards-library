8bc9a0f65d9e13457604f108130eb79255189723
Pedro Pinheiro <ph.santos.pinheiro@hotmail.com>
2021-07-27 17:24:37 -0300

    Rquisitos 3 a 5

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,1 +1,18 @@
-// implement MovieCard component
+/* eslint-disable react/prop-types */
+import React from 'react';
+
+class MovieCard extends React.Component {
+  render() {
+    const { movies } = this.props;
+    return (
+      <div>
+        <h1>{ movies.title }</h1>
+        <h3>{ movies.subtitle }</h3>
+        <p>{ movies.storyline }</p>
+        <img src={ movies.imagePath } alt={ movies.title } />
+      </div>
+    );
+  }
+}
+
+export default MovieCard;
--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,1 +1,34 @@
-// implement MovieList component here
+import React from 'react';
+import MovieCard from './MovieCard';
+
+class MovieList extends React.Component {
+  render() {
+    const movies = [
+      {
+        title: 'Movie Title 1',
+        subtitle: 'Movie Subtitle 1',
+        storyline: 'Movie Storyline 1',
+        imagePath: 'images/movie_1',
+      },
+      {
+        title: 'Movie Title 2',
+        subtitle: 'Movie Subtitle 2',
+        storyline: 'Movie Storyline 2',
+        imagePath: 'images/movie_2',
+      },
+      {
+        title: 'Movie Title 3',
+        subtitle: 'Movie Subtitle 3',
+        storyline: 'Movie Storyline 3',
+        imagePath: 'images/movie_3',
+      },
+    ];
+    return (
+      <div>
+        {movies.map((movie) => <MovieCard movies={ movie } key={ movie.title } />)}
+      </div>
+    );
+  }
+}
+
+export default MovieList;
