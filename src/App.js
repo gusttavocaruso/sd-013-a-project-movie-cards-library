ba2a52af3c47d5091598722095db4508b66e9107
Marcelo Pantoja <pantoja.marcelo@hotmail.com>
2021-07-28 01:15:40 -0300

    cria componente rating

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ class App extends React.Component {
     return (
       <div className="App">
         <Header />
-        <MovieList moviesData={ movies } />
+        <MovieList movies={ movies } />
       </div>
     );
   }
--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -2,8 +2,17 @@ import React from 'react';
 
 class MovieCard extends React.Component {
     render() {
-        const { moviesData } = this.props;
-        return ();
+        const { movie } = this.props;
+        const {title, subtitle, storyline, imagePath, rating } = movie;
+        return (
+            <section>
+            <h4>{ title }</h4>
+            <h5>{ subtitle }</h5>
+            <p>{ storyline }</p>
+            <img src={ imagePath } alt={ title } />
+            <div><Rating rating={ rating }/></div>
+            </section>
+        );
     }
 }
 export default MovieCard;
