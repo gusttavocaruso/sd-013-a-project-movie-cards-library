65feb05dab5eba11b43c55a206e60722b1c7a57b
Marcelo Pantoja <pantoja.marcelo@hotmail.com>
2021-07-28 01:55:11 -0300

    adiciona especificaçoes props

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import Rating from './Rating';
 
 class MovieCard extends React.Component {
@@ -16,4 +17,12 @@ class MovieCard extends React.Component {
         );
     }
 }
+MovieCard.propTypes = { movie: PropTypes.shape({
+    title: PropTypes.string,
+    subtitle: PropTypes.string,
+    storyline: PropTypes.string,
+    rating: PropTypes.number,
+    imagePath: PropTypes.string
+  }) };
+  MovieCard.defaultProps = { movie: {} };
 export default MovieCard;
--- src/components/Rating.jsx
+++ src/components/Rating.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 class Rating extends React.Component {
     render() {
@@ -8,4 +9,6 @@ class Rating extends React.Component {
         );
     }
 }
+Rating.propTypes = { rating: PropTypes.number };
+Rating.defaultProps = { Rating: 0 };
 export default Rating;
