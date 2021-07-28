cceed3e6a808a1acf21910d997237d0a43af6e49
Marcelo Pantoja <pantoja.marcelo@hotmail.com>
2021-07-28 01:32:18 -0300

    renderiza comp. MovieCard dentro de MovieList

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
-
+import MovieCard from './MovieCard';
 
 class MovieList extends React.Component {
   render() {
     const { movies } = this.props; 
-    return;
+    return (
+      <main>
+       {movies.map((movie) => <MovieCard key={ movie.title } movie={ movie }/>)} 
+      </main>
+    );
   }
 }
 
