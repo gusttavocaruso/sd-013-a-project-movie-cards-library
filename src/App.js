7b9a0a5ea60e16466eaea505a9c4ef1157a3353f
Pablo Almeida <pabloalmeidac@hotmail.com>
2021-07-28 18:31:49 -0300

    concluido requisito 3

--- src/App.js
+++ src/App.js
@@ -1,11 +1,26 @@
 import React from 'react';
 import './App.css';
 import Header from './components/Header';
+import MovieList from './components/MovieList';
 
 function App() {
+  const filmes = [
+    {
+      id: 1,
+      name: 'Troia',
+    }, {
+      id: 2,
+      name: 'Homem aranha',
+    }, {
+      id: 3,
+      name: 'Coringa',
+    }];
   return (
     <div className="App">
       <Header />
+      {filmes.map(({ name, id }) => (
+        <MovieList key={ id } movies={ name } />
+      ))}
     </div>
   );
 }
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,9 @@ import React from 'react';
 class Header extends React.Component {
   render() {
     return (
-      <p>Olá </p>
+      <header>
+        <h1>Movie Cards Library </h1>
+      </header>
     );
   }
 }
