b6af868f945fe51787f91731fb654805de8ffd68
Bruno <brunoalencarlima@outlook.com.br>
2021-07-28 23:11:09 -0300

    exer 6...

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import MovieCard from './MovieCard';
 
 class MovieList extends React.Component {
@@ -11,5 +12,14 @@ class MovieList extends React.Component {
     );
   }
 }
+MovieList.propTypes = { movies: PropTypes.arrayof(PropTypes.shape({
+  title: PropTypes.string,
+  subtitle: PropTypes.string,
+  storyline: PropTypes.string,
+  rating: PropTypes.number,
+  imagePath: PropTypes.string,
+})) };
+
+MovieList.defaultProps = { movies: [] };
 
 export default MovieList;
