e13b8f94c281eeaf013928f539055f0df263e7a0
Rafael Araujo <rafacarlosaraujo@gmail.com>
2021-07-29 01:31:42 -0300

    Requisito 3

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,1 +1,20 @@
-// implement MovieList component here
+import React from 'react';
+
+class MovieList extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>Teste</h1>
+      </div>
+    )
+  }
+}
+
+// 3 - Crie um componente <MovieList />
+// Crie um componente que represente toda a área com os cartões de filmes. <MovieList /> deve receber uma prop movies, que é um array de objetos com informações de um filme.
+
+// O que será verificado:
+
+// Renderize o componente <MovieList />
+
+export default MovieList;
