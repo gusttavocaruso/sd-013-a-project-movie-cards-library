7814f51c3054731890f8c988adff56e506459b93
Reinaldo Takashima Ouriques <reinaldotouriques@gmail.com>
2021-07-29 14:28:38 -0300

    Requisitos 3

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import './App.css';
 import Header from './components/Header';
+import movies from './data';
+import MoviesList from './components/MovieList';
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <MoviesList movies={ movies }/>
     </div>
   );
 }
