60ac30833957c2eb345bde3df9c2b8e3b281e220
Rafael Araujo <rafacarlosaraujo@gmail.com>
2021-07-29 01:43:32 -0300

    Tentando corrigir lint

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -6,15 +6,8 @@ class MovieList extends React.Component {
       <div>
         <h1>Teste</h1>
       </div>
-    )
+    );
   }
 }
-
-// 3 - Crie um componente <MovieList />
-// Crie um componente que represente toda a área com os cartões de filmes. <MovieList /> deve receber uma prop movies, que é um array de objetos com informações de um filme.
-
-// O que será verificado:
-
-// Renderize o componente <MovieList />
 
 export default MovieList;
