385a1abc2abf3ea954c876c60d3ab879c3598f4b
Danilo Uehara <daniloyuno@hotmail.com>
2021-07-29 10:59:39 -0300

    add exercs

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,1 +1,35 @@
-// implement MovieCard component here
+import React from 'react';
+import PropTypes from 'prop-types';
+import Rating from './Rating';
+
+class MovieCard extends React.Component {
+  render() {
+    const { movie } = this.props;
+    const { title, subtitle, storyline, rating, imagePath } = movie;
+    return (
+      <div className="movie-card">
+        <h4 className="movie-card-title">{ title }</h4>
+        <h5 className="movie-card-subtitle">{ subtitle }</h5>
+        <p className="movie-card-storyline">{ storyline }</p>
+        <Rating rating={ rating } />
+        <img src={ imagePath } alt={ title } className="movie-card-image" />
+      </div>
+    );
+  }
+}
+
+MovieCard.propTypes = {
+  movie: PropTypes.shape({
+    title: PropTypes.string,
+    subtitle: PropTypes.string,
+    storyline: PropTypes.string,
+    rating: PropTypes.number,
+    imagePath: PropTypes.string,
+  }),
+};
+
+MovieCard.defaultProps = {
+  movie: {},
+};
+
+export default MovieCard;
