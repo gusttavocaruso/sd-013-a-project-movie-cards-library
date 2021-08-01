a5554ab4d3b11a9c6b7c7d3d9bd108ec90a02e2e
Andre Leme <andresleme@gmail.com>
2021-08-01 01:18:48 -0300

    Requirements 8, 9, 10

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -14,10 +14,14 @@ class MovieCard extends Component {
 
   render() {
     const { movie } = this.props;
+    const { title, subtitle, storyline, imagePath, rating } = movie;
 
     return (
       <section>
-        <img src={ movie.imagePath } alt={ movie.title } />
+        <img src={ imagePath } alt={ title } />
+        <h4>{ title }</h4>
+        <h5>{ subtitle }</h5>
+        <p>{ storyline }</p>
       </section>
     );
   }
