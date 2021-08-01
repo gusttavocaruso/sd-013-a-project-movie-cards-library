17140177d13cb6dcf41aaffda7de213ddb1edc11
Andre Leme <andresleme@gmail.com>
2021-08-01 01:24:47 -0300

    Refactored MovieCard

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -13,8 +13,7 @@ class MovieCard extends Component {
   // }
 
   render() {
-    const { movie } = this.props;
-    const { title, subtitle, storyline, imagePath, rating } = movie;
+    const { movie: { title, subtitle, storyline, imagePath, rating } } = this.props;
 
     return (
       <section>
