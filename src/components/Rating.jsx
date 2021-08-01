6456b07c4d0fa4b659037cc2e1d7ef8f01d2f6a7
Andre Leme <andresleme@gmail.com>
2021-08-01 18:54:50 -0300

    Requirement 11

--- src/components/Rating.jsx
+++ src/components/Rating.jsx
@@ -1,1 +1,29 @@
-// implement Rating component here
+import React, { Component } from 'react';
+
+class Rating extends Component {
+  render() {
+    // const { movie: { rating } } = this.props;
+
+    return (
+      <p>test</p>
+    );
+  }
+}
+
+export default Rating;
+
+// Rating.propTypes = {
+//   subtitle: PropTypes.string,
+//   title: PropTypes.string,
+//   imagePath: PropTypes.string,
+//   storyline: PropTypes.string,
+//   rating: PropTypes.number,
+// };
+
+// Rating.defaultProps = {
+//   subtitle: '',
+//   title: '',
+//   imagePath: '',
+//   storyline: '',
+//   rating: 0,
+// };
