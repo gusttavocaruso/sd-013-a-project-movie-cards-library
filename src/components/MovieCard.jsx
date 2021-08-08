7c910d626f751b4ee5685333ec9182c2e840c4ff
Lucas Lima <lucassauro.lt@gmail.com>
2021-08-08 00:54:35 -0300

    Requisito 6 completo.

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,1 +1,34 @@
 // implement MovieCard component here
+import React from 'react';
+
+class MovieCard extends React.Component {
+  render() {
+    const { movie } = this.props;
+    return (
+      <div>
+        <p>
+          Title:
+          { movie.title }
+        </p>
+        <p>
+          Subtitle:
+          { movie.subtitle }
+        </p>
+        <p>
+          Storyline:
+          { movie.storyline }
+        </p>
+        <p>
+          ImagePath:
+          { movie.imagePath }
+        </p>
+        <p>
+          Rating:
+          { movie.rating }
+        </p>
+      </div>
+    );
+  }
+}
+
+export default MovieCard;
