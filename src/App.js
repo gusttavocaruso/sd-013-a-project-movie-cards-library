0e298d8fa5aef3773c9793e1c30bb59b2067f248
Lucas Lima <lucassauro.lt@gmail.com>
2021-08-08 00:27:14 -0300

    Requisito 3 completo.

--- src/App.js
+++ src/App.js
@@ -1,19 +1,22 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
+import movies from './data';
 import Header from './components/Header';
 // import MovieCard from './components/MovieCard';
-// import MovieList from './components/MovieList';
+import MovieList from './components/MovieList';
 // import Rating from './components/Rating';
 
-function App() {
-  return (
-    <>
-      <Header />
-      {/* <MovieCard />
-      <MovieList />
-      <Rating /> */}
-    </>
-  );
+class App extends Component {
+  render() {
+    return (
+      <>
+        <Header />
+        {/* <MovieCard /> */}
+        <MovieList movies={ movies } />
+        {/* <Rating /> */}
+      </>
+    );
+  }
 }
 
 export default App;
--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,1 +1,30 @@
 // implement MovieList component here
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+class MovieList extends Component {
+  render() {
+    const { movies } = this.props;
+    console.log(movies);
+    return (
+      <>
+        <h1>Renderizou</h1>
+        <p />
+      </>
+    );
+  }
+}
+
+MovieList.propTypes = {
+  movies: PropTypes.arrayOf(
+    PropTypes.shape({
+      title: PropTypes.string,
+      subtitle: PropTypes.string,
+      storyline: PropTypes.string,
+      rating: PropTypes.number,
+      imagePath: PropTypes.string,
+    }),
+  ).isRequired,
+};
+
+export default MovieList;
