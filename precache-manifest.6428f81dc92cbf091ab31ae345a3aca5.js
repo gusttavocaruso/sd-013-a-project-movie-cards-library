cb8fe9bd9965df85e73989b148f4b3d01fb7a495
Gustavo Mauricio <gmauricio207@gmail.com>
2021-08-15 11:58:04 -0300

    Updates

---
+++ precache-manifest.6428f81dc92cbf091ab31ae345a3aca5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "63888fceb858ca5f03671c1a76771f63",
+    "url": "/movie-cards-library/index.html"
+  },
+  {
+    "revision": "56472d75d3711c8257d5",
+    "url": "/movie-cards-library/static/css/main.6db8c1df.chunk.css"
+  },
+  {
+    "revision": "2a1eed9a2b00e9aa1252",
+    "url": "/movie-cards-library/static/js/2.56277052.chunk.js"
+  },
+  {
+    "revision": "56472d75d3711c8257d5",
+    "url": "/movie-cards-library/static/js/main.abd24f23.chunk.js"
+  },
+  {
+    "revision": "a95db5ba2439b13bdc08",
+    "url": "/movie-cards-library/static/js/runtime-main.a37d5318.js"
+  }
+]);
