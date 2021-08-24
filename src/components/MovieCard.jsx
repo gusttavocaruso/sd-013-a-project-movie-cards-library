ded444e315af8be0e5be3927de44feefa78a5bf8
João Victor Paduam <joao.paduam@hotmail.com>
2021-08-23 21:11:50 -0300

    Inicio do Projeto

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,1 +1,11 @@
-// implement MovieCard component here
+import React from 'react';
+
+class MovieCard extends React.Component {
+  render() {
+    return (
+      <div>oi</div>
+    );
+  }
+}
+
+export default MovieCard;
--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,1 +1,19 @@
-// implement MovieList component here
+import React from 'react';
+import { arrayOf } from 'prop-types';
+import MovieCard from './MovieCard';
+
+class MovieList extends React.Component {
+  render() {
+    const { movies } = this.props;
+    return (
+      <div>
+        { movies.map((movie) => <MovieCard key={ movie.title } movie={ movie } />)}
+      </div>
+    );
+  }
+}
+MovieList.propTypes = {
+  movies: arrayOf(Object).isRequired,
+};
+
+export default MovieList;
