d4283cdac61ebda7d6d4b472bad393aafe071690
João Victor Paduam <joao.paduam@hotmail.com>
2021-08-24 21:21:45 -0300

    Finalizacao do Projeto

--- src/components/Rating.jsx
+++ src/components/Rating.jsx
@@ -1,1 +1,23 @@
-// implement Rating component here
+import React, { Component } from 'react';
+import { number } from 'prop-types';
+
+class Rating extends Component {
+  render() {
+    const { rating } = this.props;
+
+    return (
+      <div>
+        <p>{ rating }</p>
+      </div>
+    );
+  }
+}
+
+Rating.propTypes = {
+  rating: number,
+};
+Rating.defaultProps = {
+  rating: 0,
+};
+
+export default Rating;
