afe607e13fe9134541933e7df812710883cda1c2
Max Salewski <maxsalewski@gmail.com>
2021-08-27 05:30:33 -0300

    Requisitos 7, 8, 9 e 10

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -7,7 +7,10 @@ class MovieCard extends React.Component {
     const { title, imagePath, subtitle, storyline } = movie;
     return (
       <div>
-        renderizou
+        <h4>{ title }</h4>
+        <h5>{ subtitle }</h5>
+        <p>{ storyline }</p>
+        <img src={ imagePath } alt={ title } />
       </div>
     );
   }
