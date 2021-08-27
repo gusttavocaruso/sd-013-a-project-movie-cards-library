30d08e824e65a1469f97a5aad801282a2d725328
Yuri <yurifranco18@live.com>
2021-08-27 12:36:56 -0300

    foi resolvido problemas do lint

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -3,29 +3,33 @@ import PropTypes from 'prop-types';
 import MovieCard from './MovieCard';
 
 class MovieList extends React.Component {
-    render() {
-        const { movies } = this.props;
-        return (
-            <div className="movie-list">
-            {movies.map((movie) => <MovieCard key={movie.title} movie={movie} />)}
-            </div>
-        )
-    }
+  render() {
+    const { movies } = this.props;
+    return (
+      <div className="movie-list">
+        {movies.map((movie) => (
+          <MovieCard key={ movie.title } movie={ movie } />
+        ))}
+      </div>
+    );
+  }
 }
 
 MovieList.PropTypes = {
-    movies: PropTypes.arrayOf(PropTypes.shape({
-        title: PropTypes.string,
-        subtitle: PropTypes.string,
-        storyline: PropTypes.string,
-        rating: PropTypes.number,
-        imagePath: PropTypes.string,
-        link: PropTypes.string,
-    }))
-}
+  movies: PropTypes.arrayOf(
+    PropTypes.shape({
+      title: PropTypes.string,
+      subtitle: PropTypes.string,
+      storyline: PropTypes.string,
+      rating: PropTypes.number,
+      imagePath: PropTypes.string,
+      link: PropTypes.string,
+    }),
+  ),
+};
 
 MovieList.defaultProps = {
-    movies = [],
+  movies: [],
 };
 
 export default MovieList;
