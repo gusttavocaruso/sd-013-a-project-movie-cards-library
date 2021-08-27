3a5fa2826a1c01ac4d4ee48c71c1082697255ed2
Eduardomak <eduardomak92@gmail.com>
2021-08-27 09:40:02 -0300

    Adicionando o resto dos requisitos, com erro no 12

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,1 +1,39 @@
-// implement MovieCard component here
+import React from 'react';
+import PropTypes from 'prop-types';
+import Rating from './Rating';
+
+class MovieCard extends React.Component {
+  render() {
+    const { movie } = this.props;
+    const { title, subtitle, storyline, rating, imagePath } = movie;
+    return (
+      <section>
+        <div>
+          <img src={ imagePath } alt={ title } />
+          <h4>{title}</h4>
+          <h5>{subtitle}</h5>
+          <p>{storyline}</p>
+          <div>
+            <Rating rating={ rating } />
+          </div>
+        </div>
+      </section>
+    );
+  }
+}
+
+MovieCard.propTypes = {
+  movie: PropTypes.arrayOf(PropTypes.shape({
+    imagePath: PropTypes.string.isRequired,
+    title: PropTypes.string.isRequired,
+    subtitle: PropTypes.string.isRequired,
+    storyline: PropTypes.string.isRequired,
+    rating: PropTypes.number.isRequired,
+  })),
+};
+
+MovieCard.defaultProps = {
+  movie: [],
+};
+
+export default MovieCard;
